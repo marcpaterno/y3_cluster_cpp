93a063056e95f51046e650424fc9bfcd3a1d9365
Marc Paterno <paterno@fnal.gov>
2018-04-07 11:49:33 -0500

    Apply clang-format

--- test/trivial_gamma_t.cc
+++ test/trivial_gamma_t.cc
@@ -6,14 +6,24 @@
 #include <string>
 #include <vector>
 
-double constexpr pi() { return 4. * std::atan(1.0); };
-double constexpr invsqrt2pi() { return 1./std::sqrt(2.*pi()); };
+double constexpr pi()
+{
+  return 4. * std::atan(1.0);
+};
+double constexpr invsqrt2pi()
+{
+  return 1. / std::sqrt(2. * pi());
+};
 
 class mz_power_law {
 public:
-  mz_power_law(double A, double B, double C) : log2_A_(std::log2(A)), B_(B), C_(C) {}
-  double operator()(double M, double z) const {
-    double const log2_res = B_ * std::log2(M) + C_* std::log2(1+z) + log2_A_;
+  mz_power_law(double A, double B, double C)
+    : log2_A_(std::log2(A)), B_(B), C_(C)
+  {}
+  double
+  operator()(double M, double z) const
+  {
+    double const log2_res = B_ * std::log2(M) + C_ * std::log2(1 + z) + log2_A_;
     return std::exp2(log2_res);
   }
 
@@ -23,37 +33,39 @@ private:
   double C_;
 };
 
-inline
-double gaussian(double x, double mu, double sigma)
+inline double
+gaussian(double x, double mu, double sigma)
 {
-  double const z = (x-mu)/sigma;
-  return std::exp(-z*z/2.) * 0.3989422804014327 / sigma;
+  double const z = (x - mu) / sigma;
+  return std::exp(-z * z / 2.) * 0.3989422804014327 / sigma;
 }
-
 
 struct HMF_t {
   double
   operator()(double x, double y) const
   {
-    //return x * y;
+    // return x * y;
     return 1.0;
   }
 };
 
 class MOR_t {
-  public:
-    MOR_t(mz_power_law l, double sigma, double alpha) : _lambda(l), _sigma(sigma), _alpha(alpha) {}
+public:
+  MOR_t(mz_power_law l, double sigma, double alpha)
+    : _lambda(l), _sigma(sigma), _alpha(alpha)
+  {}
 
   double
   operator()(double lt, double M, double zt) const
   {
     double const ltm = _lambda(M, zt);
     double const x = lt - ltm;
-    double const erfarg = -1.0 * _alpha * (x)/(std::sqrt(2.)*_sigma);
+    double const erfarg = -1.0 * _alpha * (x) / (std::sqrt(2.) * _sigma);
     double const erfterm = std::erfc(erfarg);
     return gaussian(x, 0.0, _sigma) * erfterm;
   }
-  private:
+
+private:
   mz_power_law _lambda;
   double _sigma;
   double _alpha;
@@ -63,14 +75,14 @@ struct LO_LC_t {
   double
   operator()(double x, double y, double z) const
   {
-    //return x * y + z;
+    // return x * y + z;
     return 1.0;
   }
 };
 
 class LC_LT_t {
-  public:
-    explicit LC_LT_t(double sigma) : _sigma(sigma) { }
+public:
+  explicit LC_LT_t(double sigma) : _sigma(sigma) {}
 
   double
   operator()(double lc, double lt) const
@@ -78,14 +90,15 @@ class LC_LT_t {
     double const x = lc - lt;
     return gaussian(x, 0., _sigma);
   }
-  private:
+
+private:
   double _sigma;
 };
-class
-ZO_ZT_t {
-  public:
-  explicit ZO_ZT_t(double sigma) : _sigma(sigma) { }
-  
+
+class ZO_ZT_t {
+public:
+  explicit ZO_ZT_t(double sigma) : _sigma(sigma) {}
+
   double
   operator()(double zo, double zt) const
   {
@@ -93,7 +106,7 @@ ZO_ZT_t {
     return gaussian(x, 0., _sigma);
   }
 
-  private:
+private:
   double _sigma;
 };
 
@@ -104,7 +117,7 @@ public:
   double
   operator()(double x) const
   {
-    //return x - offset;
+    // return x - offset;
     return 1.0;
   }
 
@@ -116,7 +129,7 @@ struct T_CEN_t {
   double
   operator()(double x, double y) const
   {
-    //return x + y;
+    // return x + y;
     return 1.0;
   }
 };
@@ -125,7 +138,7 @@ struct T_MIS_t {
   double
   operator()(double x, double y, double z) const
   {
-    //return x + y * z;
+    // return x + y * z;
     return 1.0;
   }
 };
@@ -134,7 +147,7 @@ struct A_CEN_t {
   double
   operator()(double a, double b, double c, double d) const
   {
-    //return (a + b) * (c + d);
+    // return (a + b) * (c + d);
     return 1.0;
   }
 };
@@ -143,7 +156,7 @@ struct A_MIS_t {
   double
   operator()(double a, double b, double c, double d, double e) const
   {
-    //return (a + b + c) * (d + e);
+    // return (a + b + c) * (d + e);
     return 1.0;
   }
 };
@@ -152,7 +165,7 @@ struct DEL_SIG_CEN_t {
   double
   operator()(double x, double y) const
   {
-    //return 3. * x + y;
+    // return 3. * x + y;
     return 1.0;
   }
 };
@@ -161,14 +174,18 @@ struct DEL_SIG_MIS_t {
   double
   operator()(double x, double y, double z) const
   {
-    //return (2. * x + 0.5 * y) * z;
+    // return (2. * x + 0.5 * y) * z;
     return 1.0;
   }
 };
 
 template <class ALG, class F>
 void
-  time_integration(ALG alg, F f, double epsrel, double epsabs, char const* algname)
+time_integration(ALG alg,
+                 F f,
+                 double epsrel,
+                 double epsabs,
+                 char const* algname)
 {
   auto start = std::chrono::high_resolution_clock::now();
   auto res = alg.integrate(f, epsrel, epsabs);
@@ -180,15 +197,15 @@ void
 int
 main(int argc, char* argv[])
 {
-  std::vector<std::string> args { argv+1, argv+argc };
+  std::vector<std::string> args{argv + 1, argv + argc};
   if (args.size() != 1) {
-	std::cerr << "Please specify an integer maxeval\n";
-}
+    std::cerr << "Please specify an integer maxeval\n";
+  }
   long long maxeval = std::stoll(args[0]);
-  MOR_t mor { mz_power_law{ 1., 1., 0.1 }, 1., 1. };
+  MOR_t mor{mz_power_law{1., 1., 0.1}, 1., 1.};
   LO_LC_t lo_lc;
-  LC_LT_t lc_lt { 1.0 };
-  ZO_ZT_t zo_zt { 0.1 };
+  LC_LT_t lc_lt{1.0};
+  ZO_ZT_t zo_zt{0.1};
   ROFFSET_t roffset{2.0};
   T_CEN_t t_cen;
   T_MIS_t t_mis;
@@ -216,7 +233,7 @@ main(int argc, char* argv[])
   double const epsrel = 1.0e-3;
   double const epsabs = 1.0e-12;
   cubacpp::Cuhre c;
-  c.maxeval = maxeval; 
+  c.maxeval = maxeval;
   time_integration(c, gti, epsrel, epsabs, "cuhre");
 
   cubacpp::Vegas v;
