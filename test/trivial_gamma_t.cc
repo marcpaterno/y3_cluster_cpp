a264cc42b4718984abf73594ab5d548ea7eb437a
Spencer <spencerweverett@gmail.com>
2018-04-11 20:01:00 +0000

    Implemented the first few cosmosis data block constructors for PDF implementations.

--- test/trivial_gamma_t.cc
+++ test/trivial_gamma_t.cc
@@ -1,5 +1,7 @@
 #include "cubacpp/cubacpp.hh"
 #include "gamma_t.hh"
+#include "/cosmosis/cosmosis/datablock/datablock.hh"
+#include "/cosmosis/cosmosis/datablock/section_names.h"
 #include <chrono>
 #include <cmath>
 #include <iostream>
@@ -66,8 +68,13 @@ private:
 class MOR_t {
 public:
   MOR_t(mz_power_law l, double sigma, double alpha)
-    : _lambda(l), _sigma(sigma), _alpha(alpha)
-  {}
+    : _lambda(l), _sigma(sigma), _alpha(alpha) {}
+
+  explicit MOR_t(cosmosis::DataBlock &sample) {
+    sample.get_val<double>("MOR_params", "lambda", _lambda);
+    sample.get_val<double>("MOR_params", "sigma", _sigma);
+    sample.get_val<double>("MOR_params", "alpha", _alpha);
+  }
 
   double
   operator()(double lt, double M, double zt) const
@@ -90,6 +97,13 @@ public:
   explicit LO_LC_t(double alpha, double a, double b, double R_lambda) :
     _alpha(alpha), _a(a), _b(b), _R_lambda(R_lambda) {}
 
+  explicit LO_LC_t(cosmosis::DataBlock &sample) {
+    sample.get_val<double>("LO_LC_params", "alpha", _alpha);
+    sample.get_val<double>("LO_LC_params", "a", _a);
+    sample.get_val<double>("LO_LC_params", "b", _b);
+    sample.get_val<double>("LO_LC_params", "R_lambda", _R_lambda);
+  }
+
   double
   operator()(double lo, double lc, double R_mis) const
   {
@@ -110,14 +124,21 @@ private:
 class LC_LT_t {
 public:
   explicit LC_LT_t(double tau, double mu, double sigma, double fmsk, double fprj)
-	  : _tau(tau), _mu(mu), _sigma(sigma), _fmsk(fmsk), _fprj(fprj) 
-  {}
+	  : _tau(tau), _mu(mu), _sigma(sigma), _fmsk(fmsk), _fprj(fprj) {}
+
+  explicit LC_LT_t(cosmosis::DataBlock &sample) {
+    sample.get_val<double>("LC_LT_params", "tau", _tau);
+    sample.get_val<double>("LC_LT_params", "mu", _mu);
+    sample.get_val<double>("LC_LT_params", "sigma", _sigma);
+    sample.get_val<double>("LC_LT_params", "fmsk", _fmsk);
+    sample.get_val<double>("LC_LT_params", "fprj", _fprj);
+  }
 
   double
   operator()(double lc, double lt, double zt) const
   {
-    return (1.0-_fmsk)*(1.0-_fprj)*invsqrt2pi()*std::exp(-std::pow((lc-_mu),2.0)/(2.0*std::pow(_sigma,2.0)))/_sigma\
-        +0.5*((1.0-_fmsk)*_fprj*_tau+_fmsk*_fprj/lt)*std::exp(_tau*(2.0*_mu+_tau*std::pow(_sigma,2.0)-2.0*lc)/2.0)*std::erfc((_mu+_tau*std::pow(_sigma,2.0)-lc)/(std::sqrt(2.0)*_sigma))\
+    return (1.0-_fmsk) * (1.0-_fprj) * invsqrt2pi() * std::exp(-std::pow((lc-_mu), 2.0) / 
+	   (2.0 *_sigma * _sigma )) / _sigma +0.5*((1.0-_fmsk)*_fprj*_tau+_fmsk*_fprj/lt)*std::exp(_tau*(2.0*_mu+_tau*std::pow(_sigma,2.0)-2.0*lc)/2.0)*std::erfc((_mu+_tau*std::pow(_sigma,2.0)-lc)/(std::sqrt(2.0)*_sigma))\
         +_fmsk*(std::erfc((_mu-lc-lt)/(std::sqrt(2.0)*_sigma))-std::erfc((_mu-lc)/(std::sqrt(2.0)*_sigma)))/(2.0*lt)\
         -_fmsk*_fprj*(std::exp(-_tau*lt)*std::exp(_tau*(2.0*_mu+_tau*std::pow(_sigma,2.0)-2.0*lc)/2.0)*std::erfc((_mu+_tau*std::pow(_sigma,2.0)-lc-lt)/(std::sqrt(2.0)*_sigma)))/(2.0*lt);
   }
@@ -133,6 +154,10 @@ private:
 class ZO_ZT_t {
 public:
   explicit ZO_ZT_t(double sigma) : _sigma(sigma) {}
+
+  explicit ZO_ZT_t(cosmosis::DataBlock &sample) {
+    sample.get_val<double>("ZO_ZT_params", "sigma", _sigma);
+  }
 
   double
   operator()(double zo, double zt) const
