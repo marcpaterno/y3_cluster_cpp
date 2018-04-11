e5b26b837f02eaf344ec0f7b5e6e8574cd9bff52
lpk1202 <lpk226551@gmail.com>
2018-04-11 19:14:33 +0000

    OMEGA_Z_t

--- test/trivial_gamma_t.cc
+++ test/trivial_gamma_t.cc
@@ -226,11 +226,25 @@ public:
 
 class OMEGA_Z_t {
 public:
+  explicit OMEGA_Z_t(double zpivot) : _zpivot(zpivot) {}
+
   double
-  operator()(double x) const
+  operator()(double zt) const
   {
-    return 1.0;
+    double poly_coeff_vol[12]={ -1.14293122E05, 5.96846869E04, 9.24239180E03, -2.23118813E03, \
+                                -4.52580713E03, 1.18404878E03, 1.27951911E02, -5.05716847E01, \
+                                1.01744577E00,  -3.11253383E-01, 5.48481084E-03,   3.12629987E00};
+    int poly_deg= 12;
+    double omega_z= 0.0;
+
+    for(int i=0; i<12; i++){
+      omega_z=omega_z+ poly_coeff_vol[i]*std::pow(zt- _zpivot,poly_deg-i-1);
+    }
+    return omega_z;
   }
+
+private:
+  double _zpivot;
 };
 
 
