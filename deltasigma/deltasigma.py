ae86e4035980d24e5652a7bc3a9e5e1f156764eb
lpk1202 <peikail@andrew.cmu.edu>
2018-05-16 21:45:13 -0500

    Delta Sigma (with 2-halo term, no boost factor)

---
+++ deltasigma/deltasigma.py
@@ -0,0 +1,93 @@
+import os
+from cosmosis.datablock import option_section, names
+import numpy as np
+from scipy.interpolate import interp1d
+from scipy.interpolate import RectBivariateSpline
+import scipy.special
+import cluster_toolkit as ct
+
+cosmo = names.cosmological_parameters
+
+def setup(options):
+	section = option_section
+
+	#saveOutput = int(options[section, "saveOutput"])
+
+	#Mpc/h comoving distance, distance on the sky
+	R_perp_min = options[section,"R_perp_min"]
+	R_perp_max = options[section,"R_perp_max"]
+	R_perp_bins = int(options[section,"R_perp_bins"])
+
+	# radii of xi_hm, in Mpc/h 
+	Radii_min = options[section,"Radii_min"]
+	Radii_max = options[section,"Radii_max"]
+	Radii_bins = int(options[section,"Radii_bins"])
+
+	#mass (float): Halo mass Msun/h.
+	M_min = options[section,"M_min"]
+	M_max = options[section,"M_max"]
+	M_bins = int(options[section,"M_bins"])
+
+	return  R_perp_min, R_perp_max, R_perp_bins, Radii_min, Radii_max, Radii_bins, M_min, M_max, M_bins
+
+def execute(block,config):
+	R_perp_min, R_perp_max, R_perp_bins, Radii_min, Radii_max, Radii_bins, M_min, M_max, M_bins = config 
+
+	omega_m = block[cosmo,"omega_m"]
+	n_s = block[cosmo,"n_s"]
+	omega_b = block[cosmo,"omega_b"]
+	h0 = block[cosmo,"h0"]
+
+	k = block["matter_power_lin", "k_h"]
+	P_k = block["matter_power_lin", "p_k"]
+	
+	k_nl = block["matter_power_nl", "k_h"]
+	P_k_nl = block["matter_power_nl", "p_k"]
+
+	z= block["matter_power_lin", "z"]
+	nz=len(z)
+
+	R_perp = np.logspace(np.log10(R_perp_min),np.log10(R_perp_max),R_perp_bins)
+	Radii = np.logspace(np.log10(Radii_min),np.log10(Radii_max),Radii_bins)
+	M = np.logspace(np.log10(M_min),np.log10(M_max),M_bins)
+
+	Size1 = M_bins * R_perp_bins
+	Deltasigma_1 = np.ndarray(Size1)
+	Deltasigma_1.shape = (M_bins,R_perp_bins)
+
+	#concentration (double): concentration coeffient in miscentering
+	concentration = 5
+
+	for i in range(M_bins):
+		xi_nfw = ct.xi.xi_nfw_at_R(Radii, M[i], concentration, omega_m)
+		Sigma1 = ct.deltasigma.Sigma_at_R(R_perp, Radii, xi_nfw, M[i], concentration, omega_m)
+		Deltasigma1 = ct.deltasigma.DeltaSigma_at_R(R_perp, Radii, Sigma1, M[i], concentration, omega_m)
+		Deltasigma_1[i] = Deltasigma1
+	
+	Size2 = nz * R_perp_bins
+	Deltasigma_2 = np.ndarray(Size2)
+	Deltasigma_2.shape = (nz,R_perp_bins)
+	
+	for i in range(nz):
+		xi_mm = ct.xi.xi_mm_at_R(Radii, k_nl, P_k_nl[i]) 
+		Sigma2 = ct.deltasigma.Sigma_at_R(R_perp, Radii, xi_mm, M[0], concentration, omega_m)
+		Deltasigma2 = ct.deltasigma.DeltaSigma_at_R(R_perp, Radii, Sigma2, M[0], concentration, omega_m)
+		Deltasigma_2[i] = Deltasigma2
+
+	Size3 = nz * M_bins
+	Bias = np.ndarray(Size3)
+	Bias.shape = (M_bins,nz)
+
+	for i in range(M_bins):
+		for j in range(nz):
+			Bias[i][j] = ct.bias.bias_at_M(M[i], k, P_k[j], omega_m)
+
+	block[cosmo, "deltasigma_1"] = Deltasigma_1
+	block[cosmo, "deltasigma_2"] = Deltasigma_2
+	block[cosmo, "bias"] = Bias
+	block[cosmo, "m_h"] = M
+	block[cosmo, "R_perp"] = R_perp
+	return 0
+
+def cleanup(config):
+	pass
--- test/trivial_gamma_t.cc
+++ test/trivial_gamma_t.cc
@@ -157,6 +157,68 @@ main(int argc, char* argv[])
   if (da_arr.empty())
     return 1;
 
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // TEST CODE FOR DELTA SIGMA////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  std::vector<double> del_sig_1;
+  std::ifstream file5(
+    "/cosmosis/cosmosis-standard-library/y3_cluster_cpp/deltasigma/deltasigma/cosmological_parameters/deltasigma_1.txt");
+  while (file5 >> num) {
+    del_sig_1.push_back(num);
+  }
+  if (del_sig_1.empty())
+    return 1;
+
+  std::vector<double> del_sig_2;
+  std::ifstream file6(
+    "/cosmosis/cosmosis-standard-library/y3_cluster_cpp/deltasigma/deltasigma/cosmological_parameters/deltasigma_2.txt");
+  while (file6 >> num) {
+    del_sig_2.push_back(num);
+  }
+  if (del_sig_2.empty())
+    return 1;
+
+  std::vector<double> bm;
+  std::ifstream file7(
+    "/cosmosis/cosmosis-standard-library/y3_cluster_cpp/deltasigma/deltasigma/cosmological_parameters/bias.txt");
+  while (file7 >> num) {
+    bm.push_back(num);
+  }
+  if (bm.empty())
+    return 1;
+
+  std::vector<double> mh1;
+  std::ifstream file8(
+    "/cosmosis/cosmosis-standard-library/y3_cluster_cpp/deltasigma/deltasigma/cosmological_parameters/m_h.txt");
+  while (file8 >> num) {
+    mh1.push_back(std::log(num));
+  }
+  if (mh1.empty())
+    return 1;
+
+  std::vector<double> r_perp;
+  std::ifstream file9(
+    "/cosmosis/cosmosis-standard-library/y3_cluster_cpp/deltasigma/deltasigma/cosmological_parameters/r_perp.txt");
+  while (file9 >> num) {
+    r_perp.push_back(num);
+  }
+  if (r_perp.empty())
+    return 1;
+
+  std::vector<double> zz1;
+  std::ifstream file10(
+    "/cosmosis/cosmosis-standard-library/y3_cluster_cpp/deltasigma/deltasigma/matter_power_lin/z.txt");
+  while (file10 >> num) {
+    zz1.push_back(num);
+  }
+  if (zz1.empty())
+    return 1;
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
   long long maxeval = std::stoll(args[0]);
   y3_cluster::MOR_t mor{mz_power_law{1.e-14, 1., 0.1}, 1., 1.};
   y3_cluster::LO_LC_t lo_lc{1.66, 0.26, 1.43, 1.0};
@@ -168,9 +230,12 @@ main(int argc, char* argv[])
   A_CEN_t a_cen;
   A_MIS_t a_mis;
   auto p1 = std::make_shared<Interp2D const>(mh, zz, dndlnmh);
+  auto p2 = std::make_shared<Interp2D const>(r_perp, mh1, del_sig_1);
+  auto p3 = std::make_shared<Interp2D const>(r_perp, zz1, del_sig_2);
+  auto p4 = std::make_shared<Interp2D const>(zz1, mh1, bm);
   y3_cluster::HMF_t hmf(p1, 0.037, 1.008);
   // DEL_SIG_CEN_t dsc{5., 0.5};
-  y3_cluster::DEL_SIG_CEN_t dsc{5.};
+  y3_cluster::DEL_SIG_CEN_t dsc(p2, p3, p4, 5.);
   // DEL_SIG_MIS_t dsc{5., 0.5};
   DEL_SIG_MIS_t dsm;
 
