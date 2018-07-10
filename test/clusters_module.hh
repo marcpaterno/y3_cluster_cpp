2234fc0c53aa1c058c0af3639a6d26f8b86f3819
Spencer <spencerweverett@gmail.com>
2018-07-10 02:11:22 +0000

    Fixed final linking issues; all builds and tests are now successful!

--- test/clusters_module.hh
+++ test/clusters_module.hh
@@ -2,6 +2,7 @@
 #define Y3CLUSTER_CLUSTERS_MODULE_HH
 
 #include "/cosmosis/cosmosis/datablock/datablock.hh"
+#include "/cosmosis/cubacpp/cubacpp/cubacpp.hh"
 #include "gamma_t.hh"
 
 namespace y3_cluster {
@@ -12,13 +13,13 @@ namespace y3_cluster {
     void execute(cosmosis::DataBlock& sample) const;
 
   private:
-    Gamma_T_Integrand<MODELS> integrand_;
+    Gamma_T_Integrand<MODELS> _integrand;
   };
 }
 
 template <class MODELS>
 y3_cluster::ClustersModule<MODELS>::ClustersModule(cosmosis::DataBlock& config)
-  : integrand_(config)
+  : _integrand(config)
 {}
 
 template <class MODELS>
@@ -30,6 +31,31 @@ y3_cluster::ClustersModule<MODELS>::execute(cosmosis::DataBlock& sample) const
   // auto val = integrand_.evaluate(sample);
   // Placeholder...
   // sample.put_val("gamma_t", "likelihood", val);
+  // FIXME: Just a test placeholder!
+  double const epsrel = 1.0e-3;
+  double const epsabs = 1.0e-12;
+  cubacpp::Cuhre c;
+  c.maxeval = 100000000;
+
+  auto result = c.integrate(_integrand, epsrel, epsabs);
+
+  std::cout << result;
+
+  //sample.put_val("gamma_t", "likelihood", result);
+
+//  time_integration(c, gti, epsrel, epsabs, "cuhre");
+//  time_integration(ALG alg,
+//  		 F f,
+//  		 double epsrel,
+//  		 double epsabs,
+//  		 char const* algname)
+//  {
+//    auto start = std::chrono::high_resolution_clock::now();
+//    auto res = alg.integrate(f, epsrel, epsabs);
+//    auto stop = std::chrono::high_resolution_clock::now();
+//    std::chrono::duration<double> diff = stop - start;
+//    std::cout << algname << ": " << res << " (" << diff.count() << "s)\n";
+//  }
 }
 
 #endif
--- test/example_module.cc
+++ test/example_module.cc
@@ -11,32 +11,3 @@
 // GENERATE_Y3_COSMOSIS_MODULE(Y3Models)
 
 GENERATE_Y3_COSMOSIS_MODULE(y3_cluster::DefaultModels)
-
-/*
-#include "test/clusters_module.hh"
-
-extern "C" { 
-
-void * setup(cosmosis::DataBlock * options) 
-{ 
-    return new y3_cluster::ClustersModule<y3_cluster::DefaultModels>(*options); 
-} 
-
-DATABLOCK_STATUS execute(cosmosis::DataBlock * block, void * config) 
-{ 
-    auto mod = static_cast<y3_cluster::ClustersModule<y3_cluster::DefaultModels> *>(config); 
-    mod->execute(*block); 
-
-    DATABLOCK_STATUS status = DBS_SUCCESS; 
-
-    return status; 
-} 
-
-int cleanup(void * config) 
-{ 
-    delete static_cast<y3_cluster::ClustersModule<y3_cluster::DefaultModels> *>(config); 
-    return 0;
-} 
-
-}
-*/
