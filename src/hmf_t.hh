d975ca63ee21474ae2637ba9a9c431a01df5a9b8
Spencer <spencerweverett@gmail.com>
2018-07-17 03:17:54 +0000

    Integrand construction is now done in `execute()`, and is no longer a class member.

--- src/hmf_t.hh
+++ src/hmf_t.hh
@@ -23,12 +23,7 @@ namespace y3_cluster {
       : _nmz([sample] () mutable {
 	// FIXME: Make this grab the hmf array from cosmosis!
 	  auto identity = [](double x) { return x; };
-	  std::cout << "Has gamma_t section: " << sample.has_section("gamma_t") << std::endl;
-	  std::cout << "Has cosmo section: " << sample.has_section(COSMOLOGICAL_PARAMETERS_SECTION) << std::endl;
-	  std::cout << "Has omega_m: " << sample.has_val("cosmological_parameters", "omega_m") << std::endl;
-	  std::cout << sample.view<double>("cosmological_parameters", "omega_m") << std::endl;
 	  double om = get_datablock<double>(sample, "cosmological_parameters", "omega_m");
-	  //double om = 0.3;
 	  auto log_omega_m = [om](double x) { return std::log(x*om); };
 	  auto mh = read_vector("m_h.txt", log_omega_m);
 	  auto const zz = read_vector("z.txt", identity);
