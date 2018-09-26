09b690f124410e275d51e2b21b80785664978d76
Yuanyuan Zhang <ynzhang@fnal.gov>
2018-09-26 10:47:10 -0500

    commiting new datablock_reader header to get rid of redundant function

--- src/datablock_reader.hh
+++ src/datablock_reader.hh
@@ -8,54 +8,6 @@
 #include "/cosmosis/cosmosis/datablock/datablock_status.h"
 #include "/cosmosis/cosmosis/datablock/ndarray.hh"
 
-inline const char *
-datablock_status_str(DATABLOCK_STATUS dbs)
-{
-  switch (dbs) {
-    case DBS_SUCCESS:
-      return "DBS_SUCCESS";
-    case DBS_DATABLOCK_NULL:
-      return "DBS_DATABLOCK_NULL";
-    case DBS_SECTION_NULL:
-      return "DBS_SECTION_NULL";
-    case DBS_SECTION_NOT_FOUND:
-      return "DBS_SECTION_NOT_FOUND";
-    case DBS_NAME_NULL:
-      return "DBS_NAME_NULL";
-    case DBS_NAME_NOT_FOUND:
-      return "DBS_NAME_NOT_FOUND";
-    case DBS_NAME_ALREADY_EXISTS:
-      return "DBS_NAME_ALREADY_EXISTS";
-    case DBS_VALUE_NULL:
-      return "DBS_VALUE_NULL";
-    case DBS_WRONG_VALUE_TYPE:
-      return "DBS_WRONG_VALUE_TYPE";
-    case DBS_MEMORY_ALLOC_FAILURE:
-      return "DBS_MEMORY_ALLOC_FAILURE";
-    case DBS_SIZE_NULL:
-      return "DBS_SIZE_NULL";
-    case DBS_SIZE_NONPOSITIVE:
-      return "DBS_SIZE_NONPOSITIVE";
-    case DBS_SIZE_INSUFFICIENT:
-      return "DBS_SIZE_INSUFFICIENT";
-    case DBS_NDIM_NONPOSITIVE:
-      return "DBS_NDIM_NONPOSITIVE";
-    case DBS_NDIM_OVERFLOW:
-      return "DBS_NDIM_OVERFLOW";
-    case DBS_NDIM_MISMATCH:
-      return "DBS_NDIM_MISMATCH";
-    case DBS_EXTENTS_NULL:
-      return "DBS_EXTENTS_NULL";
-    case DBS_EXTENTS_MISMATCH:
-      return "DBS_EXTENTS_MISMATCH";
-    case DBS_LOGIC_ERROR:
-      return "DBS_LOGIC_ERROR";
-    case DBS_USED_DEFAULT:
-      return "DBS_USED_DEFAULT";
-  }
-  return "INVALID DATA BLOCK STATUS";
-}
-
 template<typename T>
 T
 get_datablock(cosmosis::DataBlock& db, const char * section, const char * value)
@@ -72,3 +24,4 @@ template<> cosmosis::ndarray<double>
 get_datablock(cosmosis::DataBlock& db, const char * section, const char * value);
 
 #endif // _Y3_CLUSTER_CPP_DATABLOCK_READER_HH
+
