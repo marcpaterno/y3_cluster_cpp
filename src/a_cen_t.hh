b5ca21d4ac9cf276134b0e1fa9505aeb61f84442
Jackson O'Donnell <jacksonhodonnell@gmail.com>
2018-11-28 18:00:34 -0600

    first-pass triaxiality implementation

--- src/a_cen_t.hh
+++ src/a_cen_t.hh
@@ -2,6 +2,7 @@
 #define Y3_CLUSTER_A_CEN_T_HH
 
 #include "/cosmosis/cosmosis/datablock/datablock.hh"
+#include "polynomial.hh"
 
 namespace y3_cluster {
 
@@ -11,10 +12,13 @@ namespace y3_cluster {
       explicit A_CEN_t(cosmosis::DataBlock&) {}
 
       double
-      operator()(double, double, double, double) const
+      operator()(double mu) const
       {
-        // Will update with a model eventually!
-        return 1.0;
+        // Coefficients are best-fit vals from
+        // "Effects of Cluster Triaxiality on DES Stacked Weak Lensing Analysis"
+        // Zhang, Wu, and Zhang, (in preparation)
+        static constexpr polynomial<4> acen{{{0.637, -0.191, 0.182, -0.191}}};
+        return acen(mu);
       }
   };
 
