77937a00be81410ed39192d91a2ded9edc552791
Jackson O'Donnell <jacksonhodonnell@gmail.com>
2019-01-18 13:07:10 -0600

    zs runs in cosmosis

--- src/sigma_crit_inverse_t.hh
+++ src/sigma_crit_inverse_t.hh
@@ -21,18 +21,15 @@ namespace y3_cluster {
                      get_datablock<std::vector<double>>(sample, "distances", "d_a")))
     {}
 
-    sigma_crit_inv(const sigma_crit_inv&) = default;
-    sigma_crit_inv(sigma_crit_inv&&) = default;
-
     double
     operator()(double zt, double zs) const
     {
       double _sig_crit_inv = 0;
       if (zs > zt) {
-      // Units in Mpc, M_sol, s
+        // Units in Mpc, M_sol, s
         double const da_zt = _da->eval(zt), // da_z needs to be in Mpc
                      da_zs = _da->eval(zs), // da_z needs to be in Mpc
-		     // TODO: Check this - is likely wrong!
+                     // TODO: Check this - is likely wrong!
                      da_zt_zs = da_zs - (1.0+zt)/(1.0+zs) *da_zt; // da_z needs to be in Mpc
         _sig_crit_inv = 4.0*pi()*G()/c()/c() * da_zt * da_zt_zs / da_zs;
       }
