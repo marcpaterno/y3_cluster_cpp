f0aa1c987ee40801a5c4b224763090fa077f1df2
Jackson O'Donnell <jacksonhodonnell@gmail.com>
2019-02-06 17:44:02 -0600

    add verbosity, profiling options

--- src/datablock_reader.hh
+++ src/datablock_reader.hh
@@ -20,6 +20,19 @@ get_datablock(cosmosis::DataBlock& db, const char * section, const char * value)
     return a;
 }
 
+// Default version
+template<typename T>
+T
+get_datablock(cosmosis::DataBlock& db, const char * section, const char * value, T default_)
+{
+    T a;
+    auto retval = db.get_val<T>(section, value, a);
+    if (retval != DBS_SUCCESS) {
+        return default_;
+    }
+    return a;
+}
+
 template<>
 inline cosmosis::ndarray<double>
 get_datablock(cosmosis::DataBlock& db, const char * section, const char * value)
@@ -30,6 +43,19 @@ get_datablock(cosmosis::DataBlock& db, const char * section, const char * value)
         if (typ != DBT_DOUBLEND)
             retval = DBS_WRONG_VALUE_TYPE;
         throw std::runtime_error("Error (" + std::string(datablock_status_str(retval)) + ") when retrieving: " + std::string(section) + ":" + std::string(value));
+    }
+    return db.view<cosmosis::ndarray<double>>(section, value);
+}
+
+// Default version
+template<>
+inline cosmosis::ndarray<double>
+get_datablock(cosmosis::DataBlock& db, const char * section, const char * value, cosmosis::ndarray<double> default_)
+{
+    datablock_type_t typ;
+    auto retval = db.get_type(section, value, typ);
+    if (retval != DBS_SUCCESS || typ != DBT_DOUBLEND) {
+        return default_;
     }
     return db.view<cosmosis::ndarray<double>>(section, value);
 }
