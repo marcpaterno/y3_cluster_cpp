6487d8f8973c7b8c269f2d1e1c294950333373cf
Yuanyuan Zhang <ynzhang@fnal.gov>
2019-05-02 00:25:41 -0500

    implementing SDSS cosmology (without using zo_zt)

--- src/y1_analysis.cc
+++ src/y1_analysis.cc
@@ -6,6 +6,10 @@
 #include "cubacpp/integration_volume.hh"
 
 #include "lc_lt_t.hh"
+#include "mor_t2.hh"
+#include "hmf_t.hh"
+#include "dv_do_dz_t.hh"
+#include "omega_z_sdss.hh"
 #include <optional>
 #include <vector>
 using namespace y3_cluster;
@@ -35,12 +39,16 @@ private:
   // of integration volume for our integrand. If we were to change the
   // number of arguments required by the function call operator (below),
   // we would need to also modify this type alias to keep consistent.
-  using volume_t = cubacpp::IntegrationVolume<3>;
+  using volume_t = cubacpp::IntegrationVolume<4>;
 
   // State obtained from each sample.
   // If there were a type X that did not have a default constructor,
   // we would use std::optional<X> as our data member.
   std::optional<LC_LT_t> lc_lt;
+  std::optional<MOR_t2> mor;
+  std::optional<OMEGA_Z_SDSS> omega_z_sdss;
+  std::optional<DV_DO_DZ_t> dv_do_dz;
+  std::optional<HMF_t> hmf;
 
 public:
   // Initialize my integrand object from the parameters read
@@ -56,7 +64,7 @@ public:
   // integration routine does not work for functions of one variable). The
   // function is const because calling it does not change the state of the
   // object.
-  std::vector<double> operator()(double lc, double lt, double zt) const;
+  std::vector<double> operator()(double lo, double lt, double zt, double lnM) const;
 
   // finalize_sample() is where products can be put into the cosmosis::DataBlock
   // representing the current sample. The object 'sample' passed to this
@@ -89,7 +97,7 @@ namespace {
   std::string const modulelabel("y1_analysis");
 } // anonymous namespace
 
-y1_analysis::y1_analysis(DataBlock&) : lc_lt() {}
+y1_analysis::y1_analysis(DataBlock&) : lc_lt(), mor(), omega_z_sdss(), dv_do_dz(), hmf() {}
 
 void
 y1_analysis::set_sample(DataBlock& sample)
@@ -98,15 +106,20 @@ y1_analysis::set_sample(DataBlock& sample)
   // value using std::optional::emplace(...) here. emplace takes a set
   // of arguments that it passes to the constructor of X.
   lc_lt.emplace(sample);
+  mor.emplace(sample);
+  dv_do_dz.emplace(sample);
+  hmf.emplace(sample);
+  omega_z_sdss.emplace(sample);
 }
 
 std::vector<double>
-y1_analysis::operator()(double lc, double lt, double zt) const
+y1_analysis::operator()(double lo, double lt, double zt, double lnM) const
 {
   // For any data members of type std::optional<X>, we have to use operator*
   // to access the X object (as if we were dereferencing a pointer).
-  std::vector<double> results(1);
-  results[0] = (*lc_lt)(lc, lt, zt);
+  std::vector<double> results(2);
+  results[0] = (*lc_lt)(lo, lt, zt)  * (*mor)(lt, lnM, zt) * (*dv_do_dz)(zt) * (*hmf)(lnM, zt) * (*omega_z_sdss)(zt);
+  results[1] = std::exp(lnM) * (*lc_lt)(lo, lt, zt)  * (*mor)(lt, lnM, zt) * (*dv_do_dz)(zt) * (*hmf)(lnM, zt) * (*omega_z_sdss)(zt);
   return results;
 }
 
@@ -120,17 +133,30 @@ y1_analysis::finalize_sample(
   std::vector<double> N_vals;
   std::vector<double> N_errors;
   std::vector<double> N_probs;
-  std::vector<int> N_status;
+  std::vector<int> NM_status;
+  
+  std::vector<double> M_vals;
+  std::vector<double> M_errors;
+  std::vector<double> M_probs;
+
   for (auto const& result : results) {
     N_vals.push_back(result.value[0]);
     N_errors.push_back(result.error[0]);
     N_probs.push_back(result.prob[0]);
-    N_status.push_back(result.status);
+    NM_status.push_back(result.status);
+
+    M_vals.push_back(std::log10(result.value[1]/result.value[0]) );
+    M_errors.push_back(result.error[1]);
+    M_probs.push_back(result.prob[1]);
   }
   sample.put_val(modulelabel, "N_vals", N_vals);
   sample.put_val(modulelabel, "N_errors", N_errors);
   sample.put_val(modulelabel, "N_probs", N_probs);
-  sample.put_val(modulelabel, "N_status", N_status);
+  sample.put_val(modulelabel, "NM_status", NM_status);
+
+  sample.put_val(modulelabel, "M_vals", M_vals);
+  sample.put_val(modulelabel, "M_errors", M_errors);
+  sample.put_val(modulelabel, "M_probs", M_probs);
 }
 
 // The implementation of make_integration_volumes can be almost the same for
@@ -145,7 +171,7 @@ y1_analysis::make_integration_volumes(cosmosis::DataBlock& cfg)
 {
   try {
     return y3_cluster::make_integration_volumes(
-      cfg, modulelabel, "lo", "lt", "zt");
+      cfg, modulelabel, "lo", "lt", "zt", "lnm");
   }
   catch (std::exception const& ex) {
     std::cerr << ex.what();
