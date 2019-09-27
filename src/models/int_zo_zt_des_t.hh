f37ccaf27188e9efc48758650043d0ce09f2a978
matthewkirby <matthewkirby@email.arizona.edu>
2019-09-27 14:33:31 -0500

    Wrote tests for int_zo_zt_des, moved sigma_photoz_des to a new model and wrote tests for it.

--- src/models/int_zo_zt_des_t.hh
+++ src/models/int_zo_zt_des_t.hh
@@ -2,38 +2,26 @@
 #define Y3_CLUSTER_INT_ZO_ZT_HH
 
 #include "utils/datablock_reader.hh"
+#include "models/sigma_photoz_des.hh"
 #include <cmath>
 
 namespace y3_cluster {
 
-  class INT_ZO_ZT_DES_t {
-  public:
-    explicit INT_ZO_ZT_DES_t(){}
+    class INT_ZO_ZT_DES_t {
+    public:
+        explicit INT_ZO_ZT_DES_t() {}
 
+        double
+        operator()(double zomin, double zomax, double zt) const {
+            double _sigma = _sigma_photoz_des(zt);
+            double base = std::sqrt(2) * _sigma;
+            return (std::erf((zomax - zt) / base) -
+                    std::erf((zomin - zt) / base)) / 2.0;
+        }
 
-    double
-    operator()(double zomin, double zomax, double zt) const
-    {
-
-      double poly_coeff[]={ -40358.8315,   2798.08304,   9333.80185,  -657.348248,  -840.565610,   46.8506649,   37.8839498,  -0.868811858,  -0.808928182,   0.00890199353,   0.0139811265};
-      using std::erf;
-      double _sigma=0;
-      double z_for_fit=zt;
-      if (z_for_fit<0.15) { z_for_fit=0.15; } // no extrapolation outside data range
-      if (z_for_fit>0.7)  { z_for_fit=0.7; }// no extrapolation outside data range
-	
-      z_for_fit = z_for_fit - 0.4;      
-      for (int ii=0;ii<10;ii++){      
-      _sigma = (poly_coeff[ii]+_sigma)*z_for_fit; 
-      }
-      _sigma=_sigma+poly_coeff[10];
-      double base = std::sqrt(2) * _sigma;
-      return (erf((zomax - zt) / base) -
-              erf((zomin - zt) / base)) / 2.0;
-    }
-
-  private:
-  };
+    private:
+        y3_cluster::SIGMA_PHOTOZ_DES_t _sigma_photoz_des;
+    };
 }
 
 #endif
---
+++ test/zo_zt_des_t.test.cc
@@ -0,0 +1,58 @@
+#include "catch2/catch.hpp"
+#include "models/sigma_photoz_des.hh"
+#include "models/int_zo_zt_des_t.hh"
+
+#include <fstream>
+#include <string>
+#include <iostream>
+
+using y3_cluster::INT_ZO_ZT_DES_t;
+using y3_cluster::SIGMA_PHOTOZ_DES_t;
+
+TEST_CASE("All zo_zt_des objects works")
+{
+    std::ifstream infile {"../data/test_all_zo_zt_des.txt"};
+
+    // Test that we can open the file
+    REQUIRE(infile.good());
+
+    // Initialize vectors to hold truth
+    std::vector<double> zmin, zmax, zt;
+    std::vector<double> sigma_true, int_true;
+    std::string headerline;
+
+    // Read in the truth
+    std::getline(infile, headerline);
+    std::getline(infile, headerline);
+    while(infile) {
+        double onezmin, onezmax, onezt;
+        double onesig, onepdf, oneint;
+        infile >> onezmin >> onezmax >> onezt;
+        infile >> onesig >> oneint;
+        zmin.push_back(onezmin);
+        zmax.push_back(onezmax);
+        zt.push_back(onezt);
+        sigma_true.push_back(onesig);
+        int_true.push_back(oneint);
+    }
+
+    // Ensure that we have the some number of values in each column
+    REQUIRE(zmin.size() == zmax.size());
+    REQUIRE(zmax.size() == zt.size());
+    REQUIRE(zt.size() == sigma_true.size());
+    REQUIRE(zt.size() == int_true.size());
+ 
+    // Create an instance of each class
+    SIGMA_PHOTOZ_DES_t sigma_photoz_des;
+    INT_ZO_ZT_DES_t intzozt;
+
+    // Test the model against truth
+    double constexpr epsrel = 1.0e-6;
+    double constexpr epsabs = 1.0e-12;
+    for(std::size_t i=0, len=int_true.size(); i!=len; ++i) {
+        double const sigma_test = sigma_photoz_des(zt[i]);
+        double const int_test = intzozt(zmin[i], zmax[i], zt[i]);
+        CHECK(sigma_test == Approx(sigma_true[i]).epsilon(epsrel).margin(epsabs));
+        CHECK(int_test == Approx(int_true[i]).epsilon(epsrel).margin(epsabs));
+    }
+}
