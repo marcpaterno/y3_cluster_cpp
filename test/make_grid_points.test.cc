d4e953f850ef5444053b8369bd61b51f4343e976
Marc Paterno <paterno@fnal.gov>
2019-10-08 20:36:46 -0500

    Apply clang-format

--- test/make_grid_points.test.cc
+++ test/make_grid_points.test.cc
@@ -1,6 +1,6 @@
+#include "catch2/catch.hpp"
 #include "cosmosis/datablock/datablock.hh"
 #include "utils/make_grid_points.hh"
-#include "catch2/catch.hpp"
 
 #include <array>
 #include <string>
@@ -17,9 +17,10 @@ TEST_CASE("1d grid")
   {
     cosmosis::DataBlock cfg;
     cfg.put_val(module_label, "radii", std::vector<double>{3.0});
-    auto grid = y3_cluster::make_grid_points_cartesian_product(cfg, module_label, "radii");
+    auto grid = y3_cluster::make_grid_points_cartesian_product(
+      cfg, module_label, "radii");
     CHECK(grid.size() == 1);
-    grid_t expected( {{3.0}} );
+    grid_t expected({{3.0}});
     CHECK(grid == expected);
   }
 
@@ -27,7 +28,8 @@ TEST_CASE("1d grid")
   {
     cosmosis::DataBlock cfg;
     cfg.put_val(module_label, "radii", std::vector<double>{5.0, 10.0});
-    auto grid = y3_cluster::make_grid_points_cartesian_product(cfg, module_label, "radii");
+    auto grid = y3_cluster::make_grid_points_cartesian_product(
+      cfg, module_label, "radii");
     CHECK(grid.size() == 2);
     grid_t expected({{{5.0}}, {{10.0}}});
     CHECK(grid == expected);
@@ -44,7 +46,8 @@ TEST_CASE("2d grid")
     cosmosis::DataBlock cfg;
     cfg.put_val(module_label, "radii", std::vector<double>{3.0, 4.0});
     cfg.put_val(module_label, "zs", std::vector<double>{2.5});
-    auto grid = y3_cluster::make_grid_points_cartesian_product(cfg, module_label, "radii", "zs");
+    auto grid = y3_cluster::make_grid_points_cartesian_product(
+      cfg, module_label, "radii", "zs");
     CHECK(grid.size() == 2);
     grid_t expected({{{3.0, 2.5}}, {{4.0, 2.5}}});
     CHECK(grid == expected);
@@ -55,9 +58,9 @@ TEST_CASE("3d grid")
 {
   using grid_point_t = std::array<double, 3>;
   using grid_t = std::vector<grid_point_t>;
-  std::vector<double> xs {1.0, 2.0, 3.0};
-  std::vector<double> ys {4.0, 5.0};
-  std::vector<double> zs {6.0, 7.0, 8.0, 9.0};
+  std::vector<double> xs{1.0, 2.0, 3.0};
+  std::vector<double> ys{4.0, 5.0};
+  std::vector<double> zs{6.0, 7.0, 8.0, 9.0};
 
   grid_t grid = y3_cluster::detail::make_grid_splatted(xs, ys, zs);
   CHECK(grid.size() == 3 * 2 * 4);
