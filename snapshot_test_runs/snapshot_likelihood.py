3d33d8c2107838c23e3688f7da2a034d12307801
Yuanyuan Zhang <ynzhang@fnal.gov>
2019-10-25 16:00:40 -0500

    fixing error files and likelihood function

--- snapshot_test_runs/snapshot_likelihood.py
+++ snapshot_test_runs/snapshot_likelihood.py
@@ -1,7 +1,7 @@
 import numpy as np
 from cosmosis.datablock import names as section_names
 from cosmosis.datablock import option_section
-
+import matplotlib.pyplot as plt
 
 def setup(options):
     section = option_section
@@ -14,19 +14,22 @@ def setup(options):
     NC_file = options[section,"NC_file"]
     NCs=np.genfromtxt(NC_file)
     data_vector=assemble_vector(profiles*NCs, NCs)
-
+    print(NCs.shape, data_vector.shape, profiles.shape)
     #read in the profile_errfile, need to make this real
-    profile_file_err = options[section,"profile_err_file"]
-    profiles_err=np.genfromtxt(profile_file)
 
-    # read in the NCerr file, need to make this real
+    profile_file_err = options[section,"profile_err_file"]
+    profiles_err=np.genfromtxt(profile_file_err)
+
+    # read in the NCerr file
     NC_file_err = options[section,"NC_err_file"]
-    NCs_err=np.genfromtxt(NC_file)
+    NCs_err=np.genfromtxt(NC_file_err)
     cov_vector=assemble_vector(profiles_err*NCs_err, NCs_err)
     #covariance_file= options[section,"covariance_file"]
     #Need to make this real
-    covmat = np.diag(cov_vector)
+    covmat = np.diag(cov_vector**2)
      
+    print(NCs, NCs_err, cov_vector, profiles_err, covmat)
+
     return data_vector, covmat
 
 def execute(block, config):
@@ -41,9 +44,14 @@ def execute(block, config):
 
     delta = data_vector - model_vector
     weight = np.linalg.inv(covmat)
-    loglike = np.dot(delta, np.dot(weight, delta))
+    loglike = -0.5 * np.dot(delta, np.dot(weight, delta))
     block[section_names.likelihoods, 'SnapshotScalarLike_like'] = loglike
 
+    print(block['cluster_mass_profile', "concentration"], block['cosmological_parameters', "omega_m"], block['cosmological_parameters', "sigma_8"], loglike)
+    '''plt.plot( model_vector );
+    plt.yscale('log')
+    plt.errorbar(range(len(model_vector)), data_vector, yerr=np.sqrt(np.diag(covmat)), fmt='r.')
+    plt.show()'''
     return 0
 
 def cleanup(config):
@@ -52,7 +60,7 @@ def cleanup(config):
 
 def assemble_vector(profiles_model, NCs_model):
     averaged_profiles = profiles_model/NCs_model
-    model_vec=NCs_model[:,0]
+    model_vec=np.array([NCs_model[:,0], NCs_model[:,19], NCs_model[:,38]])
     model_vec=np.append(model_vec, averaged_profiles)
 
     return model_vec
