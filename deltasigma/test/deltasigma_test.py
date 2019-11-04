d6493388e84912b14766cc60d72623a054e4d139
Marc Paterno <paterno@fnal.gov>
2019-11-04 12:11:08 +0000

    Improve automation of buildig of physics validation document.
    To build the physics validation document, you need a substantial
    installation of R packages, as well as a working LaTeX installation.
    In addition, it is expected that you will have built all the required
    executables by running the CMake-generated build, doing so from the
    cosmosis/cosmosis-standard-library/y3_cluster_cpp directory.
    
    To build the physics validation document, cd into the
    cosmosis/cosmosis-standard-library/y3_cluster_cpp/validation_tests
    directory, and run 'make' with no arguments. Note that this build
    might take a while -- it may have to run many programs before it
    even begins to run the Rscript command that builds the document
    itself.

--- deltasigma/test/deltasigma_test.py
+++ deltasigma/test/deltasigma_test.py
@@ -27,49 +27,50 @@ def execute(block, config):
         test_mass=3.199267137797384375e+14
         test_z=0.2010101
 
-        dat=np.genfromtxt('test/xi_nfwonly.txt', delimiter=',')
+        basepath = "%s/cosmosis-standard-library/y3_cluster_cpp/deltasigma" % os.environ["COSMOSIS_SRC_DIR"]
+        dat=np.genfromtxt('%s/test/xi_nfwonly.txt' % basepath, delimiter=',')
         test_r=dat[:, 0]; test_test=dat[:, 1]
         test_interp=interp2d(Radii, M, Xi_1, kind='cubic')
         test_values=test_interp(test_r, test_mass)
-        with open('test/xi_nfwonly.out', 'w') as outf:
+        with open('%s/test/xi_nfwonly.out' % basepath, 'w') as outf:
             outf.write('r\t ytrue\t ytest\n')
             for ii in range(len(test_r)):
                 outf.write('%f\t%f\t%f\n'%(test_r[ii], test_test[ii], test_values[ii]))
 
-        dat=np.genfromtxt('test/Sigma_nfwonly.txt', delimiter=',')
+        dat=np.genfromtxt('%s/test/Sigma_nfwonly.txt' % basepath, delimiter=',')
         test_r=dat[:, 0]; test_test=dat[:, 1]
         test_interp=interp2d(RDS, M, S1, kind='cubic')
         test_values=test_interp(test_r, test_mass)
-        with open('test/Sigma_nfwonly.out', 'w') as outf:
+        with open('%s/test/Sigma_nfwonly.out' % basepath, 'w') as outf:
             outf.write('r\t ytrue\t ytest\n')
             for ii in range(len(test_r)):
                 outf.write('%f\t%f\t%f\n'%(test_r[ii], test_test[ii], test_values[ii]))
 
 
-        dat=np.genfromtxt('test/xi_mm_2halo.txt', delimiter=',')
+        dat=np.genfromtxt('%s/test/xi_mm_2halo.txt' % basepath, delimiter=',')
         test_r=dat[:, 0]; test_test=dat[:, 1]
         test_interp=interp2d(Radii, zz, Xi_2, kind='cubic')
         test_values=test_interp(test_r, test_z)
-        with open('test/xi_mm_2halo.out', 'w') as outf:
+        with open('%s/test/xi_mm_2halo.out' % basepath, 'w') as outf:
             outf.write('r\t ytrue\t ytest\n')
             for ii in range(len(test_r)):
                 outf.write('%f\t%f\t%f\n'%(test_r[ii], test_test[ii], test_values[ii]))
 
-        dat=np.genfromtxt('test/Sigma_mm_2halo.txt', delimiter=',')
+        dat=np.genfromtxt('%s/test/Sigma_mm_2halo.txt' % basepath, delimiter=',')
         test_r=dat[:, 0]; test_test=dat[:, 1]
         test_interp=interp2d(RDS, zz, S2, kind='quintic')
         test_values=test_interp(test_r, test_z)
-        with open('test/Sigma_mm_2halo.out', 'w') as outf:
+        with open('%s/test/Sigma_mm_2halo.out' % basepath, 'w') as outf:
             outf.write('r\t ytrue\t ytest\n')
             for ii in range(len(test_r)):
                 outf.write('%f\t%f\t%f\n'%(test_r[ii], test_test[ii], test_values[ii]))
 
-        dat=np.genfromtxt('test/bias.txt', delimiter=',')
+        dat=np.genfromtxt('%s/test/bias.txt' % basepath, delimiter=',')
         test_mass=dat[:, 0]; test_test=dat[:, 2]
         test_z=dat[:, 1];
         test_interp=interp2d(zz, M, Bias, kind='cubic')
         test_value=test_interp(test_z, test_mass)
-        with open('test/bias.out', 'w') as outf:
+        with open('%s/test/bias.out' % basepath, 'w') as outf:
             outf.write('M\t z\t ytrue\t ytest\n')
             for ii in range(len(test_mass)):
                 test_value=test_interp(test_z[ii], test_mass[ii])
