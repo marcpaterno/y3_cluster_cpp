030335e87137e6ee21741c28c805536ee50800b0
matthewkirby <matthewkirby@email.arizona.edu>
2019-12-10 18:15:11 -0600

    refactor: cuba integration is now done only over 2 dimensions

--- src/modules/AbundanceIntegralMLamtrue_DESxSPTModule.cc
+++ src/modules/AbundanceIntegralMLamtrue_DESxSPTModule.cc
@@ -8,12 +8,8 @@
 #include "cubacpp/integration_result.hh"
 #include "cubacpp/integration_volume.hh"
 
-#include "models/zo_zt_des_t.hh"
-#include "models/sptxdes/omega_z_y3xspt.hh"
-#include "models/dv_do_dz_t.hh"
 #include "models/hmf_t.hh"
 #include "models/lc_lt_y1_t.hh"
-#include "models/sptxdes/pxizeta_t.hh"
 #include "models/sptxdes/mor_y3xspt_t.hh"
 
 #include <optional>
@@ -21,44 +17,40 @@
 #include <string>
 #include <cmath>
 
-using namespace y3_cluster;
+using cosmosis::DataBlock;
 
-class AbundanceIntegralDESxSPT{
+class AbundanceIntegralDESxSPT {
 public:
-  // 3 varied grid parameters: lo, xi, zo
+  // 4 varied grid parameters: lo, xi, zo, gamma_field
   using grid_point_t = std::array<double, 4>;
 
 private:
   // Number of integration dimensions
-  using volume_t = cubacpp::IntegrationVolume<4>;
+  using volume_t = cubacpp::IntegrationVolume<2>;
 
   // State obtained from configuration
-  ZO_ZT_DES_t p_zo_zt;
-  OMEGA_Z_Y3XSPT omega_z;
-  LC_LT_Y1_t p_lo_ltzt;
-  PXiZeta_t p_xi_zeta;
+  y3_cluster::LC_LT_Y1_t PLamobs_LamtrueZtrue;
 
   // State obtained from each sample.
   // If there were a type X that did not have a default constructor,
   // we would use std::optional<X> as our data member.
-  std::optional<DV_DO_DZ_t> dv_dodz;
-  std::optional<HMF_t> hmf;
-  std::optional<MOR_Y3xSPT_t> p_ltzeta_lnmzt;
+  std::optional<y3_cluster::HMF_t> hmf;
+  std::optional<y3_cluster::MOR_Y3xSPT_t> PLamtrueZeta_lnMZtrue;
 
   // State set for current 'bin' to be integrated.
   double lamobs_;
-  double xi_;
+  double zeta_;
   double zobs_;
   double gamma_field_;
 
 public:
   // Initialize my integrand object from the parameters read
   // from the relevant block in the CosmoSIS ini file.
-  explicit AbundanceIntegralDESxSPT(cosmosis::DataBlock& config);
+  explicit AbundanceIntegralDESxSPT(DataBlock&);
 
   // Set any data members from values read from the current sample.
   // Do not attempt to copy the sample!.
-  void set_sample(cosmosis::DataBlock& sample);
+  void set_sample(DataBlock& sample);
 
   // Set the data for the current bin.
   void set_grid_point(grid_point_t const& grid_point);
@@ -68,7 +60,7 @@ public:
   // integration routine does not work for functions of one variable). The
   // function is const because calling it does not change the state of the
   // object.
-  double operator()(double lamtrue, double lnM200m, double ztrue, double zeta) const;
+  double operator()(double lamtrue, double lnM200m) const;
 
   // module_label() is a non-member (static) function that returns the label for
   // this module. The name this returns
@@ -81,33 +73,21 @@ public:
   // The following non-member (static) function creates a vector of integration
   // volumes (the type alias defined above) based on the parameters read from
   // the configuration block for the module.
-  static std::vector<volume_t> make_integration_volumes(
-    cosmosis::DataBlock& cfg);
+  static std::vector<volume_t> make_integration_volumes(DataBlock& cfg);
 
   // The following non-member (static) function creates a vector of grid points
   // on which the integration results are to be evaluated, based on parameters
   // read from the configuration block for the module.
-  static std::vector<grid_point_t> make_grid_points(cosmosis::DataBlock& cfg);
+  static std::vector<grid_point_t> make_grid_points(DataBlock& cfg);
 };
-
-
-// We write using declarations so that we don't have to type the namespace name
-// each time we use these names
-using cosmosis::DataBlock;
-using cosmosis::ndarray;
-using cubacpp::integration_result;
 
 // Set up integral that does not depend on sample
 AbundanceIntegralDESxSPT::AbundanceIntegralDESxSPT(DataBlock&)
-  : p_zo_zt()
-  , omega_z()
-  , p_lo_ltzt()
-  , p_xi_zeta()
-  , dv_dodz()
+  : PLamobs_LamtrueZtrue()
   , hmf()
-  , p_ltzeta_lnmzt()
+  , PLamtrueZeta_lnMZtrue()
   , lamobs_()
-  , xi_()
+  , zeta_()
   , zobs_()
   , gamma_field_()
 {}
@@ -116,9 +96,8 @@ AbundanceIntegralDESxSPT::AbundanceIntegralDESxSPT(DataBlock&)
 void
 AbundanceIntegralDESxSPT::set_sample(DataBlock& sample)
 {
-  dv_dodz.emplace(sample);
   hmf.emplace(sample);
-  p_ltzeta_lnmzt.emplace(sample);
+  PLamtrueZeta_lnMZtrue.emplace(sample);
 }
 
 // Set the grid points for the current integration
@@ -126,7 +105,7 @@ void
 AbundanceIntegralDESxSPT::set_grid_point(grid_point_t const& grid_point)
 {
   lamobs_ = grid_point[0];
-  xi_ = grid_point[1];
+  zeta_ = grid_point[1];
   zobs_ = grid_point[2];
   gamma_field_ = grid_point[3];
 }
@@ -135,17 +114,11 @@ AbundanceIntegralDESxSPT::set_grid_point(grid_point_t const& grid_point)
 // Anything initilized with std::optional needs an asterisk
 double
 AbundanceIntegralDESxSPT::operator()(double lamtrue,
-                                     double lnM200m,
-                                     double ztrue,
-                                     double zeta) const
+                                     double lnM200m) const
 {
-  return p_zo_zt(zobs_, ztrue)
-         * omega_z(ztrue)
-         * (*dv_dodz)(ztrue)
-         * (*hmf)(lnM200m, ztrue)
-         * p_lo_ltzt(lamobs_, lamtrue, ztrue)
-         * p_xi_zeta(xi_, zeta)
-         * (*p_ltzeta_lnmzt)(lamtrue, zeta, ztrue, lnM200m, gamma_field_);
+  return (*hmf)(lnM200m, zobs_)
+         * PLamobs_LamtrueZtrue(lamobs_, lamtrue, zobs_)
+         * (*PLamtrueZeta_lnMZtrue)(lamtrue, zeta_, zobs_, lnM200m, gamma_field_);
 }
 
 // Name of the module in the datablock
@@ -159,12 +132,11 @@ AbundanceIntegralDESxSPT::module_label()
 std::vector<AbundanceIntegralDESxSPT::volume_t>
 AbundanceIntegralDESxSPT::make_integration_volumes(cosmosis::DataBlock& cfg)
 {
-
   // Make some handy definitions
   auto modulelabel = AbundanceIntegralDESxSPT::module_label();
-  std::vector<std::string> dimensions{"lamtrue", "lnM200m", "ztrue", "zeta"};
+  std::vector<std::string> dimensions{"lamtrue", "lnM200m"};
   std::size_t const nvolumes = cfg.view<int>(modulelabel, "nclusters");
-  std::size_t const N = 4;
+  std::size_t const N = 2;
 
   // Initialize the arrays of bounds
   std::vector<cubacpp::array<N>> lowbounds(nvolumes);
@@ -182,24 +154,6 @@ AbundanceIntegralDESxSPT::make_integration_volumes(cosmosis::DataBlock& cfg)
   for (std::size_t ivol = 0; ivol != nvolumes; ++ivol) {
     lowbounds[ivol][1] = lnM200m_bounds[0];
     highbounds[ivol][1] = lnM200m_bounds[1];
-  }
-
-  // Bounds on ztrue are zobs +- Nsigma_ztrue*sigma_z(zobs)
-  auto zobs = get_vector_double(cfg, modulelabel, "zobs");
-  double Nsigma_ztrue = cfg.view<double>(modulelabel, "Nsigma_ztrue");
-  SIGMA_PHOTOZ_DES_t sigma_model;
-  for (std::size_t ivol = 0; ivol != nvolumes; ++ivol) {
-    double sigma_z = sigma_model(zobs[ivol]);
-    lowbounds[ivol][2] = zobs[ivol] - Nsigma_ztrue * sigma_z;
-    highbounds[ivol][2] = zobs[ivol] + Nsigma_ztrue * sigma_z;
-  }
-
-  // Bounds on zeta are xi +- Nsigma_zeta (sigma=1)
-  auto xi = get_vector_double(cfg, modulelabel, "xi");
-  double Nsigma_zeta = cfg.view<double>(modulelabel, "Nsigma_zeta");
-  for (std::size_t ivol = 0; ivol != nvolumes; ++ivol) {
-    lowbounds[ivol][3] = xi[ivol] - Nsigma_zeta;
-    highbounds[ivol][3] = xi[ivol] + Nsigma_zeta;
   }
 
   // Convert into the expected IntegrationVolume object
