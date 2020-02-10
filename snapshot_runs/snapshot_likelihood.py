1a0fc5ce3b68665bd495964abb6738dbe98d8a0a
Yuanyuan Zhang <ynzhang@fnal.gov>
2020-02-10 14:17:14 -0600

    now snapshot_max_sigma ini files run again

--- snapshot_runs/snapshot_likelihood.py
+++ snapshot_runs/snapshot_likelihood.py
@@ -6,6 +6,12 @@ import matplotlib.pyplot as plt
 def setup(options):
     section = option_section
 
+    #read in prediction names
+    profile_blockname = options[section,"profile_blockname"]
+    NCs_blockname = options[section,"NCs_blockname"]
+    snapshot_zs = options[section, "snapshot_zs"]
+    snapshot_lnm = options[section, "lnm_low"]
+    snapshot_radii = options[section, "radii"]
     #read in the profile_file, need to make this real
     profile_file = options[section,"profile_file"]
     profiles=np.genfromtxt(profile_file)
@@ -13,7 +19,7 @@ def setup(options):
     # read in the NC file, need to make this real
     NC_file = options[section,"NC_file"]
     NCs=np.genfromtxt(NC_file)
-    data_vector=assemble_vector(profiles*NCs, NCs)
+    data_vector=assemble_vector(profiles*NCs, NCs, snapshot_radii, snapshot_zs)
     print(NCs.shape, data_vector.shape, profiles.shape)
     #read in the profile_errfile, need to make this real
 
@@ -23,40 +29,43 @@ def setup(options):
     # read in the NCerr file
     NC_file_err = options[section,"NC_err_file"]
     NCs_err=np.genfromtxt(NC_file_err)
-    cov_vector=assemble_vector(profiles_err*NCs_err, NCs_err)
+    cov_vector=assemble_vector(profiles_err*NCs_err, NCs_err, snapshot_radii, snapshot_zs)
     #covariance_file= options[section,"covariance_file"]
     #Need to make this real
     covmat = np.diag(cov_vector**2)
 
-    profile_blockname = options[section,"profile_blockname"]
-    NCs_blockname = options[section,"NCs_blockname"]
 
-    return data_vector, covmat, profile_blockname, NCs_blockname
+    return data_vector, covmat, profile_blockname, NCs_blockname, snapshot_zs, snapshot_lnm, snapshot_radii
 
 def execute(block, config):
-    data_vector, covmat, profile_blockname, NCs_blockname = config
+    data_vector, covmat, profile_blockname, NCs_blockname, snapshot_zs, snapshot_lnm, snapshot_radii = config
 
     #read in the model values at this sample point. 
     profiles_model = block[profile_blockname, "vals"]
     NCs_model = block[NCs_blockname, "vals"]
-    model_vector = assemble_vector(profiles_model, NCs_model)
+    model_vector = assemble_vector(profiles_model, NCs_model, snapshot_radii, snapshot_zs)
+
     delta = data_vector - model_vector
     weight = np.linalg.inv(covmat)
     loglike = -0.5 * np.dot(delta, np.dot(weight, delta))
     block[section_names.likelihoods, 'SnapshotScalarLike_like'] = loglike
 
-    #print(block['cluster_mass_profile', "concentration"], block['cosmological_parameters', "omega_m"], block['cosmological_parameters', "sigma_8"], loglike)
-    #make_plots(3, 4, 18, model_vector, data_vector, covmat)
-    #make_plots_compare(3, 4, 18, model_vector, data_vector, covmat)
+    print(block['cluster_mass_profile', "concentration"], block['cosmological_parameters', "omega_m"], block['cosmological_parameters', "sigma_8"], loglike)
+    make_plots(3, 4, 18, model_vector, data_vector, covmat)
+    make_plots_compare(3, 4, 18, model_vector, data_vector, covmat)
     return 0
 
 def cleanup(config):
     #nothing to clean up
     return 0
 
-def assemble_vector(profiles_model, NCs_model):
+def assemble_vector(profiles_model, NCs_model, radii, snapshot_zs):
     averaged_profiles = profiles_model/NCs_model
-    model_vec=np.array([ NCs_model[:,0], NCs_model[:,18], NCs_model[:,36] ])
+    for ii in range(len(snapshot_zs)):
+        if ii == 0:
+           model_vec = NCs_model[:, 0]
+        else:
+           model_vec=np.append(model_vec, NCs_model[:,len(radii)*ii])
     model_vec=np.append(model_vec, averaged_profiles)
 
     return model_vec
