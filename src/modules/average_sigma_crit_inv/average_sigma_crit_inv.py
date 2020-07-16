7bf84991124e663ddb4d19b0ff5cd1edaa68a22e
Yuanyuan Zhang <ynzhang@fnal.gov>
2020-07-16 11:31:22 -0500

    fixing comsosis_tests ini files

--- src/modules/average_sigma_crit_inv/average_sigma_crit_inv.py
+++ src/modules/average_sigma_crit_inv/average_sigma_crit_inv.py
@@ -7,8 +7,8 @@ import astropy.io.fits as pyfits
 cosmo = names.cosmological_parameters
 
 def setup(options):
-	section = option_section
-        z_file_name=os.path.expandvars('${Y3_CLUSTER_CPP_DIR}/data/data_DESY1/test_cluster_Y1.fits'
+        section = option_section
+        z_file_name=os.path.expandvars('${Y3_CLUSTER_CPP_DIR}/data/test_cluster_Y1.fits')
         z_data=pyfits.open(z_file_name)[6].data
         z_array=z_data['z_mid']
         prob_array=z_data['bin1']
@@ -18,17 +18,17 @@ def setup(options):
         z_bins = int(options[section,"z_bins"])
         zl_array=np.linspace(z_min, z_max, z_bins)
 
-	return z_array, prob_array, zl_array
+        return z_array, prob_array, zl_array
 
 def execute(block, config):
         z_vals, prob_vals, zl_array=config
 
         G=4.51710305e-48 #Mpc^3 / M_sol / s^2
         c=9.71561189e-15 #Mpc/s
-	h0 = block[cosmo, "h0"]
-	delta_z = block["photoz", "delta_z"]
+        h0 = block[cosmo, "h0"]
+        delta_z = block["photoz", "delta_z"]
         pzs=interp1d(z_vals, prob_vals)
-        print np.sum(prob_vals)
+        print(np.sum(prob_vals))
 
         z_da=block['distances', 'z']
         da=block['distances', 'd_a'] # Mpc
