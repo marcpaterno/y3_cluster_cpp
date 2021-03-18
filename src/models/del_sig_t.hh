4f31a5bdf344e6f12733614f2beb87f531499491
Marc Paterno <paterno@fnal.gov>
2021-03-18 14:20:21 -0500

    Remove use of shared_ptr

--- src/models/del_sig_t.hh
+++ src/models/del_sig_t.hh
@@ -13,59 +13,37 @@
 namespace y3_cluster {
   class DEL_SIG_t {
   private:
-    std::shared_ptr<Interp2D const> _dsigma1;
-    std::shared_ptr<Interp2D const> _dsigma2;
-    std::shared_ptr<Interp2D const> _bias;
+    Interp2D _dsigma1;
+    Interp2D _dsigma2;
+    Interp2D _bias;
 
   public:
-    DEL_SIG_t(std::shared_ptr<Interp2D const> dsigma1,
-              std::shared_ptr<Interp2D const> dsigma2,
-              std::shared_ptr<Interp2D const> bias)
+    DEL_SIG_t(Interp2D const& dsigma1,
+              Interp2D const& dsigma2,
+              Interp2D const& bias)
       : _dsigma1(dsigma1), _dsigma2(dsigma2), _bias(bias)
     {}
 
     using doubles = std::vector<double>;
 
-    // TODO: This needs to be reading cosmosis datablock parameters
     explicit DEL_SIG_t(cosmosis::DataBlock& sample)
-      : _dsigma1(std::make_shared<Interp2D const>(
-          get_datablock<doubles>(sample,
-                                 "cluster_abundance",
-                                 "del_sig_params_x1"),
-          get_datablock<doubles>(sample,
-                                 "cluster_abundance",
-                                 "del_sig_params_y1"),
-          get_datablock<doubles>(sample,
-                                 "cluster_abundance",
-                                 "del_sig_params_z1")))
-      , _dsigma2(std::make_shared<Interp2D const>(
-          get_datablock<doubles>(sample,
-                                 "cluster_abundance",
-                                 "del_sig_params_x2"),
-          get_datablock<doubles>(sample,
-                                 "cluster_abundance",
-                                 "del_sig_params_y2"),
-          get_datablock<doubles>(sample,
-                                 "cluster_abundance",
-                                 "del_sig_params_z2")))
-      , _bias(std::make_shared<Interp2D const>(
-          get_datablock<doubles>(sample,
-                                 "cluster_abundance",
-                                 "del_sig_params_x3"),
-          get_datablock<doubles>(sample,
-                                 "cluster_abundance",
-                                 "del_sig_params_y3"),
-          get_datablock<doubles>(sample,
-                                 "cluster_abundance",
-                                 "del_sig_params_z3")))
+      : _dsigma1(sample.view<doubles>("cluster_abundance", "del_sig_params_x1"),
+                 sample.view<doubles>("cluster_abundance", "del_sig_params_y1"),
+                 sample.view<doubles>("cluster_abundance","del_sig_params_z1"))
+      , _dsigma2(sample.view<doubles>("cluster_abundance","del_sig_params_x2"),
+                 sample.view<doubles>("cluster_abundance","del_sig_params_y2"),
+                 sample.view<doubles>("cluster_abundance","del_sig_params_z2"))
+      , _bias(sample.view<doubles>("cluster_abundance", "del_sig_params_x3"),
+              sample.view<doubles>("cluster_abundance", "del_sig_params_y3"),
+              sample.view<doubles>("cluster_abundance", "del_sig_params_z3"))
     {}
 
     double
     operator()(double r, double lnM, double zt) const
     /*r in h^-1 Mpc */ /* M in h^-1 M_solar, represents M_{200} */
     {
-      double del_sig_1 = _dsigma1->eval(r, lnM);
-      double del_sig_2 = _bias->eval(zt, lnM) * _dsigma2->eval(r, zt);
+      double del_sig_1 = _dsigma1(r, lnM);
+      double del_sig_2 = _bias(zt, lnM) * _dsigma2(r, zt);
       if (del_sig_1 >= del_sig_2) {
         return (1. + zt) * (1. + zt) * (1. + zt) * del_sig_1;
       } else {
