e03a90b98670086f709d4be92cadd93c7c298d34
Marc Paterno <paterno@fnal.gov>
2021-03-20 15:14:19 -0500

    Simplifications

--- src/models/sigma_photoz_des.hh
+++ src/models/sigma_photoz_des.hh
@@ -1,18 +1,14 @@
 #ifndef Y3_CLUSTER_SIGMA_PHOTOZ_T_HH
 #define Y3_CLUSTER_SIGMA_PHOTOZ_T_HH
 
-#include <cmath>
-
 namespace y3_cluster {
 
-  class SIGMA_PHOTOZ_DES_t {
-  public:
-    explicit SIGMA_PHOTOZ_DES_t() {}
+  struct SIGMA_PHOTOZ_DES_t {
 
     double
     operator()(double zt) const
     {
-      double poly_coeff[] = {-40358.8315,
+      double constexpr poly_coeff[] = {-40358.8315,
                              2798.08304,
                              9333.80185,
                              -657.348248,
@@ -23,7 +19,7 @@ namespace y3_cluster {
                              -0.808928182,
                              0.00890199353,
                              0.0139811265};
-      double _sigma = 0;
+      double sigma = 0;
       double z_for_fit = zt;
 
       // We do not extrapolate outside of the data range
@@ -33,11 +29,11 @@ namespace y3_cluster {
       // Compute the fit at pivot (z-.4)
       z_for_fit = z_for_fit - 0.4;
       for (int ii = 0; ii < 10; ii++) {
-        _sigma = (poly_coeff[ii] + _sigma) * z_for_fit;
+        sigma = (poly_coeff[ii] + sigma) * z_for_fit;
       }
-      _sigma = _sigma + poly_coeff[10];
+      sigma = sigma + poly_coeff[10];
 
-      return _sigma;
+      return sigma;
     }
   };
 }
