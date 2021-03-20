bcdc3b6e7ea84903eba986edbe4eefd5a73dce9d
Marc Paterno <paterno@fnal.gov>
2021-03-20 15:59:19 -0500

    Remove use of shared_ptr

--- src/models/xi_max.hh
+++ src/models/xi_max.hh
@@ -4,8 +4,8 @@
 #include "cosmosis/datablock/datablock.hh"
 #include "cosmosis/datablock/ndarray.hh"
 #include "models/ez.hh"
-#include "utils/datablock_reader.hh"
 #include "utils/interp_2d.hh"
+#include "utils/make_interp_2d.hh"
 #include "utils/primitives.hh"
 
 #include <cmath>
@@ -14,15 +14,15 @@
 namespace y3_cluster {
   class XI_MAX {
   private:
-    std::shared_ptr<Interp2D const> _sigma1;
-    std::shared_ptr<Interp2D const> _sigma2;
-    std::shared_ptr<Interp2D const> _bias;
+    Interp2D _sigma1;
+    Interp2D _sigma2;
+    Interp2D _bias;
     double _om;
 
   public:
-    XI_MAX(std::shared_ptr<Interp2D const> sigma1,
-           std::shared_ptr<Interp2D const> sigma2,
-           std::shared_ptr<Interp2D const> bias,
+    XI_MAX(Interp2D const& sigma1,
+           Interp2D const& sigma2,
+           Interp2D const& bias,
            double om)
       : _sigma1(sigma1), _sigma2(sigma2), _bias(bias), _om(om)
     {}
@@ -30,33 +30,24 @@ namespace y3_cluster {
     using doubles = std::vector<double>;
 
     explicit XI_MAX(cosmosis::DataBlock& sample)
-      : _sigma1(std::make_shared<Interp2D const>(
-          get_datablock<doubles>(sample, "deltasigma", "r_xi"),
-          get_datablock<doubles>(sample, "deltasigma", "lnM"),
-          get_datablock<cosmosis::ndarray<double>>(sample,
-                                                   "deltasigma",
-                                                   "xi_1")))
-      , _sigma2(std::make_shared<Interp2D const>(
-          get_datablock<doubles>(sample, "deltasigma", "r_xi"),
-          get_datablock<doubles>(sample, "matter_power_lin", "z"),
-          get_datablock<cosmosis::ndarray<double>>(sample,
-                                                   "deltasigma",
-                                                   "xi_2")))
-      , _bias(std::make_shared<Interp2D const>(
-          get_datablock<doubles>(sample, "matter_power_lin", "z"),
-          get_datablock<doubles>(sample, "deltasigma", "lnM"),
-          get_datablock<cosmosis::ndarray<double>>(sample,
-                                                   "deltasigma",
-                                                   "bias")))
-      , _om(get_datablock<double>(sample, "cosmological_parameters", "omega_m"))
+      : _sigma1(make_Interp2D(sample, "deltasigma", "r_xi", "lnM", "xi_1"))
+      , _sigma2(make_Interp2D(sample,
+                              "deltasigma", "r_xi",
+                              "matter_power_lin", "z",
+                              "deltasigma","xi_2"))
+      , _bias(make_Interp2D(sample,
+                            "matter_power_lin", "z",
+                            "deltasigma", "lnM",
+                            "deltasigma","bias"))
+      , _om(sample.view<double>("cosmological_parameters", "omega_m"))
     {}
 
     double
     operator()(double r, double lnM, double zt) const
     /*r in h^-1 Mpc */ /* M in h^-1 M_solar, represents M_{200} */
     {
-      double _sig_1 = _sigma1->eval(r, lnM);
-      double _sig_2 = _bias->eval(zt, lnM) * _sigma2->eval(r, zt);
+      double _sig_1 = _sigma1(r, lnM);
+      double _sig_2 = _bias(zt, lnM) * _sigma2(r, zt);
       // TODO: h factor?
       double res = _sig_2;
       if (_sig_1 >= _sig_2) { res = _sig_1; }
