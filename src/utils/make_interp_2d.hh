5ac4cb2c61db5a587f0c99bbc5fbd4952f6a5193
Marc Paterno <paterno@fnal.gov>
2021-03-20 15:58:54 -0500

    Add new factory function for Interp2D

--- src/utils/make_interp_2d.hh
+++ src/utils/make_interp_2d.hh
@@ -21,6 +21,19 @@ namespace y3_cluster {
                     cfg.view<doubles>(section, y_axis_name),
                     cfg.view<cosmosis::ndarray<double>>(section, z_array_name));
   }
+
+  inline
+  Interp2D make_Interp2D(cosmosis::DataBlock& cfg,
+        char const* s1,
+        char const* x_axis_name,
+        char const* s2,
+        char const * y_axis_name,
+        char const* s3,
+        char const* z_array_name) {
+      return Interp2D(cfg.view<doubles>(s1, x_axis_name),
+                      cfg.view<doubles>(s2, y_axis_name),
+                      cfg.view<cosmosis::ndarray<double>>(s3, z_array_name));
+    }
 }
 
 #endif
