464e04a0b1d61358b5223e9beedd33bd64adb52b
Marc Paterno <paterno@fnal.gov>
2021-08-10 12:33:11 -0500

    WIP: debugging problem with grid handling

---
+++ src/utils/debugging.cc
@@ -0,0 +1,7 @@
+#include "debugging.hh"
+
+void
+user_breakpoint()
+{
+  // Do not do anything.
+}
---
+++ src/utils/debugging.hh
@@ -0,0 +1,9 @@
+#ifndef Y3_CLUSTER_CPP_UTILS_DEBUGGING_HH
+#define Y3_CLUSTER_CPP_UTILS_DEBUGGING_HH
+
+// This non-inlined function exists only to be something on which it is
+// easy to set a breakpoint. Put a call to it wherever you would like
+// to be able to stop the debugger.
+void user_breakpoint();
+
+#endif
