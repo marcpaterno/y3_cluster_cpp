0ddf7befc06c6bce62cca5cbe788eb8084b59292
Marc Paterno <paterno@fnal.gov>
2021-11-12 14:32:50 -0600

    Likelihood is -inf if any model is too small

--- y1_rerun/SigmaMort_likelihood.py
+++ y1_rerun/SigmaMort_likelihood.py
@@ -16,25 +16,36 @@ def setup(options):
     nc_covmat = nc_covmat[:4, :4]
     
     # model module     
-    '''profile_blockname = options[section,"profile_blockname"]
+    '''
+    profile_blockname = options[section,"profile_blockname"]
     NCs_blockname = options[section,"NCs_blockname"]
     snapshot_zs = options[section, "snapshot_zs"]
     snapshot_lnm = options[section, "lnm_low"]
-    snapshot_radii = options[section, "radii"]'''
+    snapshot_radii = options[section, "radii"]
+    '''
+
     rad = options[section, "radii"]
     Redges = np.logspace(np.log10(0.0323), np.log10(30.), num=15+1)
     return profiles, covmat, ncs, nc_covmat, rad, Redges
 
 def execute(block, config):
 
-    data_vector, covmat, ncs, nc_covmat, rad, Redges = config
-
     #read in the model values at this sample point. 
     profiles_miscent_model = block['SigmaMiscentY1CUDAIntegrand', "vals"]
     profiles_cent_model = block['SigmaCentY1CUDAIntegrand', "vals"]
     NC_miscent_model = block['NCMiscentY1CUDAIntegrand', "vals"]
     NC_cent_model = block['NCCentY1CUDAIntegrand', "vals"]
-    miscent_fraction = block['cluster_abundance', 'fcen'] 
+    miscent_fraction = block['cluster_abundance', 'fcen']
+
+    # TODO: Consider making the value of too_small a configuration parameter
+    # of the module.
+    too_small = 0.1
+    if (any_model_too_small(too_small, profiles_miscent_model, profiles_cent_model, NC_miscent_model, NC_cent_model)):
+        block[section_names.likelihoods, "SigmaMort_Like_like"] = -np.inf
+        return 0
+
+    # Recover what we created in setup.
+    data_vector, covmat, ncs, nc_covmat, rad, Redges = config
    
     sigma = profiles_miscent_model/NC_miscent_model * miscent_fraction + (profiles_cent_model/NC_cent_model)*(1-miscent_fraction)
     model_ncs = NC_miscent_model * miscent_fraction + (NC_cent_model)*(1-miscent_fraction)
@@ -50,6 +61,7 @@ def execute(block, config):
     weight2 = np.linalg.inv(nc_covmat)
     loglike2 = -0.5 * np.dot(delta2, np.dot(weight2, delta2))
     loglike = loglike1 + loglike2
+
     block[section_names.likelihoods, 'SigmaMort_Like_like'] = loglike
 
     #print(block['cluster_mass_profile', "concentration"], block['cosmological_parameters', "omega_m"], block['cosmological_parameters', "sigma_8"], loglike1, loglike2, loglike, delta, delta2, model_ncs, ncs, model_vector)
@@ -62,6 +74,17 @@ def execute(block, config):
 def cleanup(config):
     #nothing to clean up
     return 0
+
+def any_model_too_small(small, *models):
+    """Take any number of np.ndarray objects, and check that all entries are
+    'not too small'.
+
+    Return True if any entry is too small, and False otherwise.
+    """
+    for model in *models:
+        if np.any(model < small):
+            return True
+    return False
 
 def sig_gamma(rad, profiles, xx0):
     ds= np.zeros([profiles.shape[0], len(rad)])
