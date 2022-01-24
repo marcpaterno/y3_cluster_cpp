39ae54cde1b71a534da78891f1360f072b94e57d
yyzhang <ynzhang@umich.edu>
2022-01-24 16:21:32 -0600

    resolving conflicts

--- y1_rerun/SigmaMort_likelihood.py
+++ y1_rerun/SigmaMort_likelihood.py
@@ -9,43 +9,66 @@ def setup(options):
     section = option_section
     # data vector 
     profiles = np.genfromtxt('data_files/wl_data_vector.txt')
+    profiles_err = np.genfromtxt('data_files/wl_data_vector_err.txt')
     covmat = np.genfromtxt('data_files/wl_cov.txt')
     ncs = np.genfromtxt('data_files/nc.txt', delimiter=',')
     nc_covmat = np.genfromtxt('data_files/Cov_ij_bestfit_DESY1_105.txt')
-    ncs = ncs[0, :]
-    nc_covmat = nc_covmat[:4, :4]
-    
+    #ncs = ncs[0, :]
+    nc_covmat = nc_covmat[:12, :12]
+     
+
     # model module     
     '''profile_blockname = options[section,"profile_blockname"]
     NCs_blockname = options[section,"NCs_blockname"]
     snapshot_zs = options[section, "snapshot_zs"]
     snapshot_lnm = options[section, "lnm_low"]
     snapshot_radii = options[section, "radii"]'''
-    rad = options[section, "radii"]
+    rad = options[section, "radius"]
     Redges = np.logspace(np.log10(0.0323), np.log10(30.), num=15+1)
-    return profiles, covmat, ncs, nc_covmat, rad, Redges
+    return profiles, covmat, ncs, nc_covmat, rad, Redges, profiles_err
 
 def execute(block, config):
 
-    data_vector, covmat, ncs, nc_covmat, rad, Redges = config
+    data_array, covmat, ncs, nc_covmat, rad, Redges, data_vector_err = config
 
-    #read in the model values at this sample point. 
+    #read in the model values at this sample point.
+     
     profiles_miscent_model = block['SigmaMiscentY1MortScalarIntegrand', "vals"]
     profiles_cent_model = block['SigmaCentY1MortScalarIntegrand', "vals"]
     NC_miscent_model = block['NCMiscentY1MortScalarIntegrand', "vals"]
     NC_cent_model = block['NCCentY1MortScalarIntegrand', "vals"]
     miscent_fraction = block['cluster_abundance', 'fcen'] 
-   
+    
+    '''
+    profiles_miscent_model = np.genfromtxt('sigma_y1_output/sigmamiscenty1mortscalarintegrand/vals.txt')
+    profiles_cent_model = np.genfromtxt('sigma_y1_output/sigmacenty1mortScalarintegrand/vals.txt')
+    NC_miscent_model = np.genfromtxt('sigma_y1_output/ncmiscentY1mortscalarintegrand/vals.txt')
+    NC_cent_model = np.genfromtxt('sigma_y1_output/nccenty1mortscalarintegrand/vals.txt')
+    miscent_fraction = 0.7 #np.genfromtxt('sigma_y1_output/cluster_abundance/fcen.txt') 
+    '''
+
     sigma = profiles_miscent_model/NC_miscent_model * miscent_fraction + (profiles_cent_model/NC_cent_model)*(1-miscent_fraction)
-    model_ncs = NC_miscent_model * miscent_fraction + (NC_cent_model)*(1-miscent_fraction)
-    model_ncs = model_ncs[:, 0]
+    ncs_vector = NC_miscent_model * miscent_fraction + (NC_cent_model)*(1-miscent_fraction)
+    #sigma = profiles_miscent_model/NC_miscent_model # * miscent_fraction # + (profiles_cent_model/NC_cent_model)*(1-miscent_fraction)
+    #ncs_vector = NC_miscent_model # * miscent_fraction # + (NC_cent_model)*(1-miscent_fraction)
+    #sigma = (profiles_cent_model/NC_cent_model)
+    #ncs_vector = (NC_cent_model)
+    model_ncs = np.array([])
+    ngrid = sigma.shape[1]/len(rad)
+    ngrid = int(ngrid)
+    for kk in range(ngrid):
+        model_ncs = np.append(model_ncs, ncs_vector[:, kk*(len(rad))])
     ds, aver_ds = convert_s2ds(rad, sigma, Redges)
     model_vector = aver_ds.flatten()
-
+    
+    data_vector = data_array.flatten()
     delta = data_vector - model_vector
     weight = np.linalg.inv(covmat)
     loglike1 = -0.5 * np.dot(delta, np.dot(weight, delta))
     
+    loglike1 = 0.0
+    print(ncs.shape, model_ncs.shape, nc_covmat.shape)
+    ncs = ncs.flatten()
     delta2 = ncs - model_ncs
     weight2 = np.linalg.inv(nc_covmat)
     loglike2 = -0.5 * np.dot(delta2, np.dot(weight2, delta2))
@@ -55,7 +78,7 @@ def execute(block, config):
     #print(block['cluster_mass_profile', "concentration"], block['cosmological_parameters', "omega_m"], block['cosmological_parameters', "sigma_8"], loglike1, loglike2, loglike, delta, delta2, model_ncs, ncs, model_vector)
     #plt.plot(model_vector, 'k--');plt.plot(data_vector, 'b:');plt.show()
     #plt.plot(ncs, 'k--');plt.plot(model_ncs, 'b:');plt.show()
-    #make_plots(3, 4, 18, model_vector, data_vector, covmat)
+    #make_plots(3, 4, Redges, aver_ds, data_array,  data_vector_err, ncs, model_ncs, nc_covmat)
     #make_plots_compare(3, 4, 18, model_vector, data_vector, covmat)
     return 0
 
@@ -74,19 +97,29 @@ def sig_gamma(rad, profiles, xx0):
     return mean
 
 def convert_s2ds(rad, profiles, Redges):
-    ds= np.zeros([profiles.shape[0], len(rad)])
-    print(ds.shape, profiles.shape)
+    #print(ds.shape, profiles.shape)
+    ngrid=int(profiles.shape[1]/len(rad))
+    ds= np.zeros([profiles.shape[0]*ngrid, len(rad)])
     for jj in range(profiles.shape[0]):
-        for ii in range(len(rad)):
-            ds[jj, ii] = np.trapz(profiles[jj, :ii]*rad[:ii], rad[:ii])*2.0/(rad[ii])**2 - profiles[jj, ii]
+        for kk in range(ngrid):
+            for ii in range(len(rad)):
+                profiles_ind = kk * len(rad)
+                ds_int = np.trapz(profiles[jj, profiles_ind:(profiles_ind+ii+1)]*rad[:(ii+1)], rad[:(ii+1)])*2.0/(rad[ii])**2 - profiles[jj, (profiles_ind+ii)]
+                ds[kk*profiles.shape[0]+jj, ii] = ds_int
 
 
-    res = np.zeros([profiles.shape[0], len(Redges)-1])
-    for jj in range(profiles.shape[0]):
+    res = np.zeros([ds.shape[0], len(Redges)-1])
+    for jj in range(ds.shape[0]):
         prof_interp=interp1d(rad, ds[jj, :])
+        #prof_interp=interp1d(rad, profiles[jj, :])
         prof_func=lambda x: prof_interp(x) * x
         for ii in range(len(Redges)-1):
-            print(jj, ii, integrate.quad(prof_func, Redges[ii], Redges[ii+1])*2/((Redges[ii+1]**2) - (Redges[ii]**2)))
+            # averaging DS ranges
+            res_int, unc_int = integrate.quad(prof_func, Redges[ii], Redges[ii+1])
+            res_int = res_int *2/((Redges[ii+1]**2) - (Redges[ii]**2))
+
+            #print(jj, ii, res_int)
+            res[jj, ii] = res_int
     return ds, res
 
 
@@ -101,25 +134,45 @@ def assemble_vector(profiles_model, NCs_model, radii, snapshot_zs):
 
     return model_vec
 
-def make_plots(ngriddim, nvoldim, nrad, model_vector, data_vector, covmat):
+def make_plots(ngriddim, nvoldim, rad_edges, model_vector, data_vector, data_err, ncs, model_ncs, nc_covmat):
+
+    rad = 0.5*(rad_edges[1:]**2 - rad_edges[:-1]**2)/(rad_edges[1:] - rad_edges[:-1])
     ngriddim = int(ngriddim)
     nvoldim = int(nvoldim)
-    nrad = int(nrad)
-    unc = np.diag( np.sqrt(covmat) )
-    fig, axs = plt.subplots(ngriddim, nvoldim+1, figsize=[12, 8])
+    nrad = int(len(rad))
+    unc = data_err
+    unc_nc = np.diag( np.sqrt(nc_covmat) )
+    fig, axs = plt.subplots(2, ngriddim, figsize=[12, 8])
 
+    zbin_labels = [r"Redshift Bin [0.2;0.35)", r"Redshift Bin [0.35;0.5)", r"Redshift Bin [0.5;0.65)"]
+    lam_labels = [r"$\lambda \in [20;30)$", r"$\lambda \in [30;45)$", r"$\lambda \in [45;60)$", r"$\lambda \in [60;\infty)$"]
     for j in range(ngriddim):
+        axs[0, j].errorbar(range(nvoldim), model_ncs[j*nvoldim:j*nvoldim+nvoldim], fmt='s', color = 'tab:pink', label = 'Models')
+        axs[0, j].errorbar(range(nvoldim), ncs[j*nvoldim:j*nvoldim+nvoldim], yerr=unc_nc[j*nvoldim:j*nvoldim+nvoldim], fmt='o', color = 'gray', label = 'Data')
+        axs[0, j].set_xticks(range(nvoldim))
+        axs[0, j].set_xticklabels(lam_labels) 
         for i in range(nvoldim):
-            ndim1=j*nvoldim
-            ndim2=(j+1)*nvoldim
-            axs[j, nvoldim].plot(model_vector[ndim1:ndim2])
-            axs[j, nvoldim].errorbar(range(nvoldim), data_vector[ndim1:ndim2], yerr=unc[ndim1:ndim2], fmt='ko')
-
-            dim1 = i*ngriddim*nrad + j*nrad + ngriddim*nvoldim
-            dim2 = i*ngriddim*nrad + (j+1)*nrad + ngriddim*nvoldim
-            axs[j, i].plot(range(nrad), model_vector[dim1:dim2], 'r--')
-            axs[j, i].errorbar(range(nrad), data_vector[dim1:dim2], yerr=unc[dim1:dim2], fmt='ko')
-            axs[j, i].set_yscale('log')
+            #ndim1=j*nvoldim
+            #ndim2=(j+1)*nvoldim
+            #axs[j, nvoldim].plot(model_vector[ndim1:ndim2])
+            #print(model_ncs, ncs, unc_nc)
+            axs[1, j].plot(rad, model_vector[j*nvoldim+i], label = "Models "+lam_labels[i])
+            #print(dim1, dim2, len(unc), model_vector[dim1:dim2])
+            dim1 = i * len(rad)
+            dim2 = (i+1) * len(rad)
+            axs[1, j].errorbar(rad, data_vector[j*nvoldim+i], yerr=unc[j*nvoldim+i], fmt='o', color = 'gray')
+            axs[1, j].set_yscale('log')
+            axs[1, j].set_xscale('log')
+            axs[1, j].set_xlim([0.2, 20])
+            axs[1, j].set_ylim([2, 2000])
+            
+            axs[0, j].set_yscale('log')
+            axs[1, j].set_xlabel(r'$R$ [Comoving] Mpc/$h$')
+        axs[0, j].set_title(zbin_labels[j])
+    axs[0, 0].set_ylabel('Number Counts')
+    axs[1, 0].set_ylabel(r'$\Delta \Sigma$')
+    axs[1, 2].legend(loc = 0)
+    axs[0, 2].legend(loc = 0)
     plt.show()
 
 
--- y1_rerun/data_files/assemble_profiles.py
+++ y1_rerun/data_files/assemble_profiles.py
@@ -2,38 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
 
-nvec = 4
+nvec = 12
 ndim = 15
-rad_vectors = np.zeros(nvec*ndim) 
-data_vectors = np.zeros(nvec*ndim) 
-data_vectors_err = np.zeros(nvec*ndim) 
+rad_vectors = np.zeros([nvec, ndim]) 
+data_vectors = np.zeros([nvec, ndim]) 
+data_vectors_err = np.zeros([nvec, ndim]) 
 cov_vectors = np.zeros([nvec*ndim, nvec*ndim]) 
 
 
-for l in range(4):
-    for z in range(1):
+for z in range(3):
+    for l in range(4):
         lam = l+3
         dat=np.genfromtxt('wl_data_files/full-unblind-v2-mcal-zmix_y1subtr_l%i_z%i_profile.dat'%(lam, z));
-        print(dat)
+        print(dat[:, 0])
         cov = np.genfromtxt('SAC_files/SAC_z%i_l%i.txt'%(z, lam))
-        start = (z + l)*ndim
-        stop = (z + l)*ndim + ndim
+        start = (z*4 + l)
+        stop = (z*4 + l)+1
         print(dat.shape, cov.shape, start, stop,  np.shape(rad_vectors[start:stop]))
-        rad_vectors[start:stop]=dat[:, 0]
-        data_vectors[start:stop]=dat[:, 1]
-        data_vectors_err[start:stop]=dat[:, 2]
-        cov_vectors[start:stop, start:stop]=cov
-
-#plt.errorbar(rad_vectors, data_vectors, yerr=data_vectors_err,c='r')
+        rad_vectors[start, :]=dat[:, 0]
+        data_vectors[start, :]=dat[:, 1]
+        data_vectors_err[start, :]=dat[:, 2]
+        cov_vectors[(start*ndim):(stop*ndim), (start*ndim):(stop*ndim)]=cov
+        if z == 2:
+           plt.errorbar(rad_vectors[start, :], data_vectors[start, :], yerr=data_vectors_err[start, :])
 #plt.errorbar(rad_vectors, data_vectors, yerr=np.diag(cov_vectors),c='k')
 #plt.plot(rad_vectors, data_vectors_err, c='r')
-plt.plot(rad_vectors, data_vectors_err - np.sqrt(np.diag(cov_vectors)), '.', c='r')
+#plt.plot(rad_vectors, data_vectors_err - np.sqrt(np.diag(cov_vectors)), '.', c='r')
 #plt.plot(rad_vectors, np.sqrt(np.diag(cov_vectors)),c='k')
 plt.xscale('log')
 plt.yscale('symlog')
 plt.show()
 
 np.savetxt('wl_data_vector.txt', data_vectors)
+np.savetxt('wl_data_vector_err.txt', data_vectors_err)
 np.savetxt('wl_data_vector_radius.txt', rad_vectors)
 np.savetxt('wl_cov.txt', cov_vectors)
 imshow(np.log(cov_vectors))
