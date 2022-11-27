85626aed7c871420910b4b1639165b8ff8250d76
Johnny Esteves <johnny.udi@gmail.com>
2022-11-27 13:36:02 -0800

    set better binning scheme

--- y3_buzzard/setup_bins.py
+++ y3_buzzard/setup_bins.py
@@ -12,11 +12,20 @@ import numpy as np
 ###################################################################
 
 # redshift bins
-zbins = np.array([0.2, 0.35, 0.5, 0.65])
-zmeans = np.array([0.275, 0.4, 0.575])
-zmeans_ij = np.array([0.275, 0.4, 0.575, 0.275, 0.4, 0.575, 0.275, 0.4, 0.575, 0.275, 0.4, 0.575])
+# zbins = np.array([0.2, 0.35, 0.5, 0.65])
+# zmeans = np.array([0.275, 0.4, 0.575])
+# zmeans_ij = np.array([0.275, 0.4, 0.575, 0.275, 0.4, 0.575, 0.275, 0.4, 0.575, 0.275, 0.4, 0.575])
+
+# Buzzard Mock
+zbins = np.array([0.2, 0.32, 0.373, 0.51, 0.65])
+zmin_list = np.array([0.2, 0.373, 0.51])
+zmax_list = np.array([0.32, 0.51, 0.64])
+zmeans = np.array([0.25, 0.44, 0.575])
+zmeans_ij = np.array([0.25, 0.44, 0.575, 0.25, 0.44, 0.575, 0.25, 0.44, 0.575, 0.25, 0.44, 0.575])
 
 # lambda bins
-lbdbins =  np.array([20, 30, 45, 60, 300])
+lbdbins =  np.array([5, 15, 25, 40, 160])
 lbdmeans = 0.5*(lbdbins[1:]+lbdbins[:-1])
 lbdmeans_ij = np.repeat(lbdmeans, 3)
+
+
