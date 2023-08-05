b7b78e781700a1cb68346ea08be2db0027fc88d9
Marc Paterno <paterno@fnal.gov>
2023-08-05 09:11:43 -0700

    Fix nfw_sigma_mis.cuh

--- src/models/nfw_sigma_mis.cuh
+++ src/models/nfw_sigma_mis.cuh
@@ -9,11 +9,13 @@
 #include <math.h>
 #include <string>
 
+#include "fmt/core.h"
+
 #include "cosmosis/datablock/datablock.hh"
 
-#include "utils/make_interp_1d.hh"
 #include "utils/cuda_interp_2d.cuh"
-
+#include "utils/make_interp_1d.hh"
+#include "utils/read_vector.hh"
 
 namespace y3_cuda {
   // Default concentration value
@@ -25,63 +27,78 @@ namespace y3_cuda {
   // selects the miscentering kernel ('single','gamma')
   std::string const GAMMA = "gamma";
 
-
-  // Helper functions to construct filenames needed to read the interpolation
-  // table information.
-  inline std::string logx_file(std::string const& kernel)
-  {
-    return fmt::format("nfw_off_center/table_1000_1e-02_1e+04_{}_logx.txt", kernel);
-  }
-
-  inline std::string logxmis_file(std::string const& kernel)
-  {
-    return fmt::format("nfw_off_center/table_1000_1e-02_1e+04_{}_logxmis.txt", kernel);
-  }
-
-  inline std::string log_sigma_file(std::string const& kernel)
-  {
-    return fmt::format("nfw_off_center/table_1000_1e-02_1e+04_log_sigma_{}.txt", kernel);
-  }
-
-
   class NFW_SIGMA_MIS {
 
-    public:
+    // Helper functions to construct filenames needed to read the interpolation
+    // table information. This is so we can initialize the tables correctly,
+    // rather than attempting to default-construct them and then replace the
+    // default- constructed table with a correctly initialized one.
+
+    static inline std::string
+    logx_file(std::string const& kernel)
+    {
+      return fmt::format("nfw_off_center/table_1000_1e-02_1e+04_{}_logx.txt",
+                         kernel);
+    }
+
+    static inline std::string
+    logxmis_file(std::string const& kernel)
+    {
+      return fmt::format("nfw_off_center/table_1000_1e-02_1e+04_{}_logxmis.txt",
+                         kernel);
+    }
+
+    static inline std::string
+    log_sigma_file(std::string const& kernel)
+    {
+      return fmt::format(
+        "nfw_off_center/table_1000_1e-02_1e+04_log_sigma_{}.txt", kernel);
+    }
+
+  public:
     NFW_SIGMA_MIS(double c, double rhoc, std::string const& kernel)
-      : _c(c),
-        _rhoc(rhoc),
-        _nfwProfile(read_vector(logx_file(kernel)),
+      : _c(c)
+      , _rhoc(rhoc)
+      , _nfwProfile(read_vector(logx_file(kernel)),
                     read_vector(logxmis_file(kernel)),
                     read_vector(log_sigma_file(kernel)))
-    { }
+    {}
 
     NFW_SIGMA_MIS()
-    : _c(CONC),
-      _rhoc(RHOC),
-      _nfwProfile(read_vector(logx_file(GAMMA)),
-                  read_vector(logxmis_file(GAMMA)),
-                  read_vector(log_sigma_file(GAMMA)))
+      : _c(CONC)
+      , _rhoc(RHOC)
+      , _nfwProfile(read_vector(logx_file(GAMMA)),
+                    read_vector(logxmis_file(GAMMA)),
+                    read_vector(log_sigma_file(GAMMA)))
 
-    { }
+    {}
 
     // In case, we envolve the NFW profile with redshift
     // TODO: Implement Mass-Concentration Relation
     // TODO: Implement different operator in case of rhocz(zt)
     // Ask Marc How to make _c and _rhoc be functional forms in any case
     NFW_SIGMA_MIS(cosmosis::DataBlock& sample)
-    : _c(y3_cluster::make_Interp1D(sample,"correlationFunction","lnM","concentration").clamp(14.0))
-    , _rhoc(y3_cluster::make_Interp1D(sample,"correlationFunction","z","rhoc").clamp(0.0))
-    , _nfwProfile(read_vector(logx_file(GAMMA)),
-                  read_vector(logxmis_file(GAMMA)),
-                  read_vector(log_sigma_file(GAMMA)))
-    { }
+      : _c(y3_cluster::make_Interp1D(sample,
+                                     "correlationFunction",
+                                     "lnM",
+                                     "concentration")
+             .clamp(14.0))
+      , _rhoc(
+          y3_cluster::make_Interp1D(sample, "correlationFunction", "z", "rhoc")
+            .clamp(0.0))
+      , _nfwProfile(read_vector(logx_file(GAMMA)),
+                    read_vector(logxmis_file(GAMMA)),
+                    read_vector(log_sigma_file(GAMMA)))
+    {}
 
     __device__ __host__ double
-    operator()(double r, double rmis, double lnM) const 
+    operator()(double r, double rmis, double lnM) const
     {
-      double const delta_c = 200.0 * _c * _c * _c / 3.0 * (std::log(1.0 + _c) - _c / (1.0 + _c));
+      double const delta_c =
+        200.0 * _c * _c * _c / 3.0 * (std::log(1.0 + _c) - _c / (1.0 + _c));
       double const rho_crit = _rhoc;
-      double const r_200 = std::cbrt(3.0 * std::exp(lnM) / (800.0 * M_PI * rho_crit));
+      double const r_200 =
+        std::cbrt(3.0 * std::exp(lnM) / (800.0 * M_PI * rho_crit));
       double const r_s = r_200 / _c;
       double const norm = r_s * delta_c * rho_crit;
 
@@ -89,7 +106,7 @@ namespace y3_cuda {
       double const xmis = rmis / r_s;
 
       double const log_unfw = _nfwProfile.clamp(std::log(x), std::log(xmis));
-      
+
       double const nfw = norm * std::exp(log_unfw);
       return nfw;
     }
