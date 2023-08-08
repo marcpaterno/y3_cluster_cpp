e0b298cde40453d85ecff4f718d2f76808ab3a25
Johnny Esteves <johnny.udi@gmail.com>
2023-08-08 12:07:05 -0700

    sigma tests passed besides last point

--- src/models/nfw_sigma_mis.cuh
+++ src/models/nfw_sigma_mis.cuh
@@ -37,14 +37,14 @@ namespace y3_cuda {
     static inline std::string
     logx_file(std::string const& kernel)
     {
-      return fmt::format("nfw_off_center/offset_nfw_table_500_1e-02_1e+04_{}_logx.txt",
+      return fmt::format("nfw_off_center/table_1000_1e-03_5e+03_{}_logx.txt",
                          kernel);
     }
 
     static inline std::string
     logxmis_file(std::string const& kernel)
     {
-      return fmt::format("nfw_off_center/offset_nfw_table_500_1e-02_1e+04_{}_logxmis.txt",
+      return fmt::format("nfw_off_center/table_1000_1e-03_5e+03_{}_logxmis.txt",
                          kernel);
     }
 
@@ -52,7 +52,7 @@ namespace y3_cuda {
     log_sigma_file(std::string const& kernel)
     {
       return fmt::format(
-        "nfw_off_center/offset_nfw_table_500_1e-02_1e+04_log_sigma_{}.txt", kernel);
+        "nfw_off_center/table_1000_1e-03_5e+03_log_sigma_{}.txt", kernel);
     }
 
   public:
@@ -94,21 +94,22 @@ namespace y3_cuda {
     __device__ __host__ double
     operator()(double r, double rmis, double lnM) const
     {
-      double const delta_c =
-        200.0 * _c * _c * _c / 3.0 * (std::log(1.0 + _c) - _c / (1.0 + _c));
+      double const delta_c = (200.0 * _c * _c * _c / 3.0) / (std::log(1.0 + _c) - _c / (1.0 + _c));
       double const rho_crit = _rhoc;
       double const r_200 =
         std::cbrt(3.0 * std::exp(lnM) / (800.0 * M_PI * rho_crit));
       double const r_s = r_200 / _c;
-      double const norm = r_s * delta_c * rho_crit;
+
+      // normalization term defined in Wright & Brainerd 2000
+      double const norm = 2 * r_s * delta_c * rho_crit;
 
       double const x = r / r_s;
       double const xmis = rmis / r_s;
-
       double const log_unfw = _nfwProfile.clamp(std::log(x), std::log(xmis));
-
       double const nfw = norm * std::exp(log_unfw);
-      return nfw;
+
+      // Conversion from Msun/Mpc^2 to Msun/h pc^2
+      return nfw*1e-12;
     }
 
   private:
--- test/nfw_sigma_mis.test.cu
+++ test/nfw_sigma_mis.test.cu
@@ -1,66 +1,73 @@
 #include "catch2/catch.hpp"
 #include "models/hmf_t.hh"
-#include "utils/interp_2d.hh"
 #include "utils/make_ifstream.hh"
 #include "utils/read_vector.hh"
 
 // This is the code we're actually testing
 #include "models/nfw_sigma_mis.cuh"
+#include "utils/cuda_interp_2d.cuh"
 
 #include <fstream>
 #include <iomanip>
-#include <vector>
+#include <fmt/format.h>
 
+using y3_cluster::HMF_t;
+// using y3_cluster::Interp2D;
 using y3_cuda::NFW_SIGMA_MIS;
 
-// This test case (see https://github.com/catchorg/Catch2 for an introduction
-// to the testing framework we are using for C++).
-//
-// This test case is not typical for Catch2 because it serves two purposes:
-//   1. It is an actual unit test; see the use of the CHECK macro in the code
-//      below.
-//   2. As a side effect to running the test, a file is written that can be
-//      used in creating a section for our validation document.
-//
-
-
-// This test case exercises a default-constructed model object.
-// Maybe there is also a need to test ones made through other
-// constructors.
-TEST_CASE("Default constructed NFW_SIGMA_MIS works")
+TEST_CASE("Test NFW Misc Implementation")
 {
+  // Setup the initial conditions properly
+  double  conc = 4.0;
+  double  omega_m = 0.3;
+  double  RHOC = 2.77533742639e+11;
+  double  RHOM = RHOC*omega_m;
+  NFW_SIGMA_MIS model(conc, RHOM, "gamma");
+
+  double const epsrel = 1.0e-2;
+  double const epsabs = 1.0e-12;
+
+  // Here we need to fill the vectors rs, rmiss, lnMs and answers 
+  // Radii vector log spaced from 0.1 to 10 with 10 points
+  std::vector<double> r{ 0.1       ,  0.16681005,  0.27825594,  0.46415888,  0.77426368,
+                         1.29154967,  2.15443469,  3.59381366,  5.9948425 , 10.        };
+
+    // Answers drawn from cluster toolkit
+  // Each columns is a different rmis, each row is a different r
+  std::vector<double> answers{154.2477729, 36.2597318, 5.9036604,
+                              126.1577857, 35.0800970, 5.8699027,
+                               86.6157755, 32.5206407, 5.7884943,
+                               47.3583660, 27.6699473, 5.6047940,
+                               21.4019571, 20.1449034, 5.2255105,
+                                8.8737342, 11.4269575, 4.5301542,
+                                3.5299064, 4.7155369, 3.4469777,
+                                1.3583749, 1.5653792, 2.1094762,
+                                0.5101472, 0.5325816, 0.9306969,
+                                0.1884599, 0.1911689, 0.2793004};
+
+  std::vector<double> rmis{0.1,0.5,2.0};
+  std::vector<double> lnMs{32.2361913};
+
+  std::size_t const n_r = r.size();
+  std::size_t const n_rmiss = rmis.size();
+  std::size_t const n_lnMs = lnMs.size();
+
   auto out = y3_cluster::make_ofstream("data/nfw_sigma_mis_test.out");
   REQUIRE(out.good());
   out << std::setw(16);
   out << std::setprecision(16);
   out << "r\trmis\tlnM\tytrue\tytest\n";
-
-  // Here we need to fill the vectors rs, rmiss, lnMs and answers with the
-  // right data for the test.
-  // CURRENTLY THIS CODE DOES NOT FILL THE ARRAYS WITH RIGHT DATA.
-  std::vector<double> rs{1.0};
-  std::vector<double> rmiss{0.5};
-  std::vector<double> lnMs{10.0};
-  std::vector<double> answers{1.0};
-    
-  NFW_SIGMA_MIS model;
-  double const epsrel = 1.0e-3;
-  double const epsabs = 1.0e-12;
-
-  std::size_t const n_rs = rs.size();
-  std::size_t const n_rmiss = rmiss.size();
-  std::size_t const n_lnMs = lnMs.size();
-
-  for (std::size_t i = 0; i != n_rs; ++i) {
+  
+  for (std::size_t i = 0; i != n_r; ++i) {
     for (std::size_t j = 0; j != n_rmiss; ++j) {
       for (std::size_t k = 0; k != n_lnMs; ++k) {
-        double const calculated_result = model(rs[i], rmiss[j], lnMs[k]);
+        double const calculated_result = model(r[i], rmis[j], lnMs[k]);
         // Adjust this look to look up the answers correctly; the indexing needed will depend
         // on the order of storage of the results.
         double const expected_result = answers[k + n_lnMs*j + n_lnMs*n_rmiss*i];
         CHECK(calculated_result == Approx(expected_result).epsilon(epsrel).margin(epsabs));
-        out << rs[i] << '\t'
-            << rmiss[j] << '\t'
+        out << r[i] << '\t'
+            << rmis[j] << '\t'
             << lnMs[k] << '\t'
             << expected_result << '\t'
             << calculated_result << '\n';
@@ -68,3 +75,25 @@ TEST_CASE("Default constructed NFW_SIGMA_MIS works")
     }
   }
 }
+
+// 
+// To Generate The Answers; Go To a Python Interface with Cluster Toolkit
+// 
+// import numpy as np
+// import cluster_toolkit as ct
+
+// def make_sigma_mis(R, Rmis=0.3, Mt=1e14, c=4., omega_m=0.3):
+//     Rp = np.logspace(np.log10(0.001),np.log10(15.),300)
+//     st = ct.deltasigma.Sigma_nfw_at_R(Rp, Mt, c, omega_m)
+//     _st_mis = ct.miscentering.Sigma_mis_at_R(Rp, Rp, st, Mt, c, omega_m, Rmis, kernel='gamma')
+//     return np.interp(R,Rp,_st_mis)
+
+// R = np.logspace(np.log10(0.1),np.log10(10.),10)
+// R
+
+// res = np.vstack([make_sigma_mis(R, 0.1),
+//                  make_sigma_mis(R, 0.5),
+//                  make_sigma_mis(R, 2.)])
+
+// for row in res.T:
+//     print('%.7f, %.7f, %.7f,'%(row[0],row[1],row[2]))
